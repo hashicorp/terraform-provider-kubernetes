2c273d6d08daad11080706762a639c84161d7722
Matt Morrison <matt@mojo.net.nz>
2017-10-26 15:02:04 +1300

    Make fmt

--- kubernetes/structure_service_spec.go
+++ kubernetes/structure_service_spec.go
@@ -2,8 +2,8 @@ package kubernetes
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
-	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/pkg/api/v1"
 )
 
 // Flatteners

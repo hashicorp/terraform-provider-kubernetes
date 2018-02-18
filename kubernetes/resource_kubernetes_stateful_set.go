65bb5ae5397f26d3eb616b67ffd39a3ac213b510
Matt Morrison <matt@mojo.net.nz>
2018-02-18 21:52:42 +1300

    Update StatefulSet schema to better match native kubernetes schema. Add missing schema fields. WIP

--- kubernetes/resource_kubernetes_stateful_set.go
+++ kubernetes/resource_kubernetes_stateful_set.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	pkgApi "k8s.io/apimachinery/pkg/types"
@@ -23,6 +24,8 @@ func resourceKubernetesStatefulSet() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
+		SchemaVersion: 1,
+		MigrateState:  resourceKubernetesStatefulSetStateUpgrader,
 		Schema: map[string]*schema.Schema{
 			"metadata": namespacedMetadataSchema("statefulset", true),
 			"spec": {
@@ -32,11 +35,23 @@ func resourceKubernetesStatefulSet() *schema.Resource {
 				MaxItems:    1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
+						"pod_management_policy": {
+							Type:        schema.TypeString,
+							Description: "Controls how pods are created during initial scale up, when replacing pods on nodes, or when scaling down. The default policy is OrderedReady, where pods are created in increasing order (pod-0, then pod-1, etc) and the controller will wait until each pod is ready before continuing. When scaling down, the pods are removed in the opposite order. The alternative policy is Parallel which will create pods in parallel to match the desired scale without waiting, and on scale down will delete all pods at once.",
+							Optional:    true,
+							Default:     "OrderedReady",
+						},
 						"replicas": {
 							Type:        schema.TypeInt,
 							Description: "The number of desired replicas. Defaults to 1. More info: http://kubernetes.io/docs/user-guide/replication-controller#what-is-a-replication-controller",
 							Optional:    true,
 							Default:     1,
+						},
+						"revision_history_limit": {
+							Type:        schema.TypeInt,
+							Description: "revisionHistoryLimit is the maximum number of revisions that will be maintained in the StatefulSet's revision history. The revision history consists of all revisions not represented by a currently applied StatefulSetSpec version. The default value is 10.",
+							Optional:    true,
+							Default:     10,
 						},
 						"selector": {
 							Type:        schema.TypeMap,
@@ -50,11 +65,72 @@ func resourceKubernetesStatefulSet() *schema.Resource {
 						},
 						"template": {
 							Type:        schema.TypeList,
-							Description: "Describes the pod that will be created if insufficient replicas are detected. Each pod stamped out by the StatefulSet will fulfill this Template, but have a unique identity from the rest of the StatefulSet.",
+							Description: "Template describes the pods that will be created.",
 							Required:    true,
 							MaxItems:    1,
 							Elem: &schema.Resource{
-								Schema: podSpecFields(true),
+								Schema: map[string]*schema.Schema{
+									"metadata": metadataSchema("statefulsetSpec", true),
+									"spec": &schema.Schema{
+										Type:        schema.TypeList,
+										Description: "Template describes the pods that will be created.",
+										Required:    true,
+										MaxItems:    1,
+										Elem: &schema.Resource{
+											Schema: podSpecFields(true),
+										},
+									},
+									"active_deadline_seconds":          relocatedAttribute("active_deadline_seconds"),
+									"container":                        relocatedAttribute("container"),
+									"dns_policy":                       relocatedAttribute("dns_policy"),
+									"host_ipc":                         relocatedAttribute("host_ipc"),
+									"host_network":                     relocatedAttribute("host_network"),
+									"host_pid":                         relocatedAttribute("host_pid"),
+									"hostname":                         relocatedAttribute("hostname"),
+									"node_name":                        relocatedAttribute("node_name"),
+									"node_selector":                    relocatedAttribute("node_selector"),
+									"restart_policy":                   relocatedAttribute("restart_policy"),
+									"security_context":                 relocatedAttribute("security_context"),
+									"service_account_name":             relocatedAttribute("service_account_name"),
+									"automount_service_account_token":  relocatedAttribute("automount_service_account_token"),
+									"subdomain":                        relocatedAttribute("subdomain"),
+									"termination_grace_period_seconds": relocatedAttribute("termination_grace_period_seconds"),
+									"volume": relocatedAttribute("volume"),
+								},
+							},
+						},
+						"update_strategy": {
+							Type:        schema.TypeList,
+							Optional:    true,
+							Computed:    true,
+							Description: "updateStrategy indicates the StatefulSetUpdateStrategy that will be employed to update Pods in the StatefulSet when a revision is made to Template.",
+							MaxItems:    1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"type": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										Default:     "RollingUpdate",
+										Description: "Type indicates the type of the StatefulSetUpdateStrategy. Default is RollingUpdate.",
+									},
+									"rolling_update": {
+										Type:        schema.TypeList,
+										Description: "RollingUpdate is used to communicate parameters when Type is RollingUpdateStatefulSetStrategyType.",
+										Optional:    true,
+										Computed:    true,
+										MaxItems:    1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"partition": {
+													Type:        schema.TypeInt,
+													Description: "Partition indicates the ordinal at which the StatefulSet should be partitioned. Default value is 0.",
+													Optional:    true,
+													Default:     0,
+												},
+											},
+										},
+									},
+								},
 							},
 						},
 						"volume_claim_templates": {
@@ -80,20 +156,10 @@ func resourceKubernetesStatefulSetCreate(d *schema.ResourceData, meta interface{
 	if err != nil {
 		return err
 	}
-	spec.Template.ObjectMeta.Annotations = metadata.Annotations
 
 	//use name as label and selector if not set
-	if metadata.Name == "" {
-		metadata.Name = d.Get("name").(string)
-	}
 	if metadata.Namespace == "" {
 		metadata.Namespace = "default"
-	}
-	if len(spec.Selector.MatchLabels) == 0 {
-		spec.Selector.MatchLabels = map[string]string{
-			"app": d.Get("name").(string),
-		}
-		spec.Template.ObjectMeta.Labels = spec.Selector.MatchLabels
 	}
 
 	statefulSet := v1beta1.StatefulSet{
@@ -270,4 +336,28 @@ func waitForStatefulSetReplicasFunc(conn *kubernetes.Clientset, ns, name string)
 		return resource.RetryableError(fmt.Errorf("Waiting for %d replicas of %q to be scheduled (%d)",
 			desiredReplicas, statefulSet.GetName(), statefulSet.Status.Replicas))
 	}
+}
+
+func resourceKubernetesStatefulSetStateUpgrader(
+	v int, is *terraform.InstanceState, meta interface{}) (*terraform.InstanceState, error) {
+	if is.Empty() {
+		log.Println("[DEBUG] Empty InstanceState; nothing to migrate.")
+		return is, nil
+	}
+
+	var err error
+
+	switch v {
+	case 0:
+		log.Println("[INFO] Found Kubernetes StatefulSet State schema v0; migrating to v1")
+		is, err = migrateStateV0toV1(is)
+		if err != nil {
+			return is, err
+		}
+
+	default:
+		return is, fmt.Errorf("Unexpected schema version: %d", v)
+	}
+
+	return is, err
 }
--- kubernetes/structures_stateful_set.go
+++ kubernetes/structures_stateful_set.go
@@ -15,14 +15,23 @@ func flattenStatefulSetSpec(in v1beta1.StatefulSetSpec, d *schema.ResourceData) 
 	if in.Replicas != nil {
 		att["replicas"] = *in.Replicas
 	}
+	att["pod_management_policy"] = in.PodManagementPolicy
+	if in.RevisionHistoryLimit != nil {
+		att["revision_history_limit"] = *in.RevisionHistoryLimit
+	}
 	att["service_name"] = in.ServiceName
 	att["selector"] = in.Selector.MatchLabels
+	att["update_strategy"] = flattenStatefulSetUpdateStrategy(in.UpdateStrategy, d)
 
+	templateMetadata := flattenMetadata(in.Template.ObjectMeta, d)
 	podSpec, err := flattenPodSpec(in.Template.Spec)
 	if err != nil {
 		return nil, err
 	}
-	att["template"] = podSpec
+	template := make(map[string]interface{})
+	template["metadata"] = templateMetadata
+	template["spec"] = podSpec
+	att["template"] = []interface{}{template}
 
 	volClaimTemplates := make([]map[string]interface{}, len(in.VolumeClaimTemplates), len(in.VolumeClaimTemplates))
 	for i, claim := range in.VolumeClaimTemplates {
@@ -36,22 +45,59 @@ func flattenStatefulSetSpec(in v1beta1.StatefulSetSpec, d *schema.ResourceData) 
 	return []interface{}{att}, nil
 }
 
+func flattenStatefulSetUpdateStrategy(in v1beta1.StatefulSetUpdateStrategy, d *schema.ResourceData) []interface{} {
+	att := make(map[string]interface{})
+	if in.Type != "" {
+		att["type"] = in.Type
+	}
+	if in.RollingUpdate != nil {
+		att["rolling_update"] = flattenStatefulSetStrategyRollingUpdate(in.RollingUpdate)
+	}
+	return []interface{}{att}
+}
+
+func flattenStatefulSetStrategyRollingUpdate(in *v1beta1.RollingUpdateStatefulSetStrategy) []interface{} {
+	att := make(map[string]interface{})
+	if in.Partition != nil {
+		att["partition"] = in.Partition
+	}
+
+	return []interface{}{att}
+}
+
+//
+// EXPANDERS
+//
+
 func expandStatefulSetSpec(statefulSet []interface{}) (v1beta1.StatefulSetSpec, error) {
 	obj := v1beta1.StatefulSetSpec{}
 	if len(statefulSet) == 0 || statefulSet[0] == nil {
 		return obj, nil
 	}
 	in := statefulSet[0].(map[string]interface{})
-
+	if v, ok := in["strategy"]; ok {
+		obj.UpdateStrategy = expandStatefulSetUpdateStrategy(v.([]interface{}))
+	}
 	obj.Replicas = ptrToInt32(int32(in["replicas"].(int)))
 	obj.Selector = &metav1.LabelSelector{
 		MatchLabels: expandStringMap(in["selector"].(map[string]interface{})),
 	}
 	obj.ServiceName = in["service_name"].(string)
 
-	podSpec, err := expandPodSpec(in["template"].([]interface{}))
-	if err != nil {
-		return obj, err
+	for _, v := range in["template"].([]interface{}) {
+		template := v.(map[string]interface{})
+		podSpec, err := expandPodSpec(template["spec"].([]interface{}))
+		if err != nil {
+			return obj, err
+		}
+		obj.Template = v1.PodTemplateSpec{
+			Spec: podSpec,
+		}
+
+		if metaCfg, ok := template["metadata"]; ok {
+			metadata := expandMetadata(metaCfg.([]interface{}))
+			obj.Template.ObjectMeta = metadata
+		}
 	}
 
 	volClaimTemplates := in["volume_claim_templates"].([]interface{})
@@ -68,12 +114,35 @@ func expandStatefulSetSpec(statefulSet []interface{}) (v1beta1.StatefulSetSpec, 
 	}
 	obj.VolumeClaimTemplates = pvcTemplates
 
-	obj.Template = v1.PodTemplateSpec{
-		ObjectMeta: metav1.ObjectMeta{
-			Labels: obj.Selector.MatchLabels,
-		},
-		Spec: podSpec,
+	return obj, nil
+}
+
+func expandStatefulSetUpdateStrategy(p []interface{}) v1beta1.StatefulSetUpdateStrategy {
+	obj := v1beta1.StatefulSetUpdateStrategy{}
+	if len(p) == 0 || p[0] == nil {
+		return obj
+	}
+	in := p[0].(map[string]interface{})
+
+	if v, ok := in["type"]; ok {
+		obj.Type = v1beta1.StatefulSetUpdateStrategyType(v.(string))
+	}
+	if v, ok := in["rolling_update"]; ok {
+		obj.RollingUpdate = expandRollingUpdateStatefulSetStrategy(v.([]interface{}))
+	}
+	return obj
+}
+
+func expandRollingUpdateStatefulSetStrategy(p []interface{}) *v1beta1.RollingUpdateStatefulSetStrategy {
+	obj := v1beta1.RollingUpdateStatefulSetStrategy{}
+	if len(p) == 0 || p[0] == nil {
+		return &obj
+	}
+	in := p[0].(map[string]interface{})
+
+	if v, ok := in["partition"]; ok {
+		obj.Partition = ptrToInt32(v.(int32))
 	}
 
-	return obj, nil
+	return &obj
 }

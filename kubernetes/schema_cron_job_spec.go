3974df03a93925a220d75f664fb0ccf20db1386e
Matt Morrison <matt@mojo.net.nz>
2018-04-05 23:53:54 +1200

    Complete out CronJob schema and add tests

--- kubernetes/schema_cron_job_spec.go
+++ kubernetes/schema_cron_job_spec.go
@@ -2,15 +2,23 @@ package kubernetes
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
 )
 
 func cronJobSpecFields() map[string]*schema.Schema {
 	s := map[string]*schema.Schema{
-		"schedule": {
+		"concurrency_policy": {
 			Type:         schema.TypeString,
-			Required:    true,
-			//ValidateFunc: validate, TODO: validate cron syntax..
-			Description:  "Cron format string, e.g. 0 * * * * or @hourly, as schedule time of its jobs to be created and executed.",
+			Optional:     true,
+			Default:      "Allow",
+			ValidateFunc: validation.StringInSlice([]string{"Allow", "Forbid", "Replace"}, false),
+			Description:  "Specifies how to treat concurrent executions of a Job. Defaults to Allow.",
+		},
+		"failed_jobs_history_limit": {
+			Type:        schema.TypeInt,
+			Optional:    true,
+			Default:     "1",
+			Description: "The number of failed finished jobs to retain. This is a pointer to distinguish between explicit zero and not specified. Defaults to 1.",
 		},
 		"job_template": {
 			Type:        schema.TypeList,
@@ -18,8 +26,43 @@ func cronJobSpecFields() map[string]*schema.Schema {
 			Required:    true,
 			MaxItems:    1,
 			Elem: &schema.Resource{
-				Schema: jobSpecFields(),
+				Schema: map[string]*schema.Schema{
+					"metadata": metadataSchema("jobTemplateSpec", true),
+					"spec": {
+						Type:        schema.TypeList,
+						Description: "Specification of the desired behavior of the job",
+						Required:    true,
+						MaxItems:    1,
+						Elem: &schema.Resource{
+							Schema: jobSpecFields(),
+						},
+					},
+				},
 			},
+		},
+		"schedule": {
+			Type:     schema.TypeString,
+			Required: true,
+			//ValidateFunc: validate, TODO: validate cron syntax..
+			Description: "Cron format string, e.g. 0 * * * * or @hourly, as schedule time of its jobs to be created and executed.",
+		},
+		"starting_deadline_seconds": {
+			Type:        schema.TypeInt,
+			Optional:    true,
+			Default:     0,
+			Description: "Optional deadline in seconds for starting the job if it misses scheduled time for any reason. Missed jobs executions will be counted as failed ones.",
+		},
+		"successful_jobs_history_limit": {
+			Type:        schema.TypeInt,
+			Optional:    true,
+			Default:     3,
+			Description: "The number of successful finished jobs to retain. Defaults to 3.",
+		},
+		"suspend": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Default:     false,
+			Description: "This flag tells the controller to suspend subsequent executions, it does not apply to already started executions. Defaults to false.",
 		},
 	}
 
--- kubernetes/structure_cron_job.go
+++ kubernetes/structure_cron_job.go
@@ -3,19 +3,52 @@ package kubernetes
 import (
 	"github.com/hashicorp/terraform/helper/schema"
 	"k8s.io/api/batch/v1beta1"
-	"errors"
 )
 
 func flattenCronJobSpec(in v1beta1.CronJobSpec) ([]interface{}, error) {
 	att := make(map[string]interface{})
 
+	att["concurrency_policy"] = in.ConcurrencyPolicy
+	if in.FailedJobsHistoryLimit != nil {
+		att["failed_jobs_history_limit"] = int(*in.FailedJobsHistoryLimit)
+	} else {
+		att["failed_jobs_history_limit"] = 1
+	}
+
 	att["schedule"] = in.Schedule
 
-	jobSpec, err := flattenJobSpec(in.JobTemplate.Spec)
+	jobTemplate, err := flattenJobTemplate(in.JobTemplate)
 	if err != nil {
 		return nil, err
 	}
-	att["job_template"] = jobSpec
+	att["job_template"] = jobTemplate
+
+	if in.StartingDeadlineSeconds != nil {
+		att["starting_deadline_seconds"] = int(*in.StartingDeadlineSeconds)
+	} else {
+		att["starting_deadline_seconds"] = 0
+	}
+
+	if in.SuccessfulJobsHistoryLimit != nil {
+		att["successful_jobs_history_limit"] = int(*in.SuccessfulJobsHistoryLimit)
+	} else {
+		att["successful_jobs_history_limit"] = 3
+	}
+
+	return []interface{}{att}, nil
+}
+
+func flattenJobTemplate(in v1beta1.JobTemplateSpec) ([]interface{}, error) {
+	att := make(map[string]interface{})
+
+	meta := flattenMetadata(in.ObjectMeta)
+	att["metadata"] = meta
+
+	jobSpec, err := flattenJobSpec(in.Spec)
+	if err != nil {
+		return nil, err
+	}
+	att["spec"] = jobSpec
 
 	return []interface{}{att}, nil
 }
@@ -29,16 +62,49 @@ func expandCronJobSpec(j []interface{}) (v1beta1.CronJobSpec, error) {
 
 	in := j[0].(map[string]interface{})
 
+	obj.ConcurrencyPolicy = v1beta1.ConcurrencyPolicy(in["concurrency_policy"].(string))
+
+	if v, ok := in["failed_jobs_history_limit"].(int); ok && v != 1 {
+		obj.FailedJobsHistoryLimit = ptrToInt32(int32(v))
+	}
+
 	obj.Schedule = in["schedule"].(string)
 
-	podSpec, err := expandJobSpec(in["job_template"].([]interface{}))
+	jtSpec, err := expandJobTemplate(in["job_template"].([]interface{}))
 	if err != nil {
 		return obj, err
 	}
+	obj.JobTemplate = jtSpec
 
+	if v, ok := in["starting_deadline_seconds"].(int); ok && v > 0 {
+		obj.StartingDeadlineSeconds = ptrToInt64(int64(v))
+	}
 
-	obj.JobTemplate = v1beta1.JobTemplateSpec {
-		Spec: podSpec,
+	if v, ok := in["successful_jobs_history_limit"].(int); ok && v != 3 {
+		obj.StartingDeadlineSeconds = ptrToInt64(int64(v))
+	}
+
+	if v, ok := in["suspend"].(bool); ok {
+		obj.Suspend = ptrToBool(v)
+	}
+
+	return obj, nil
+}
+
+func expandJobTemplate(in []interface{}) (v1beta1.JobTemplateSpec, error) {
+	obj := v1beta1.JobTemplateSpec{}
+
+	tpl := in[0].(map[string]interface{})
+
+	spec, err := expandJobSpec(tpl["spec"].([]interface{}))
+	if err != nil {
+		return obj, err
+	}
+	obj.Spec = spec
+
+	if metaCfg, ok := tpl["metadata"]; ok {
+		metadata := expandMetadata(metaCfg.([]interface{}))
+		obj.ObjectMeta = metadata
 	}
 
 	return obj, nil

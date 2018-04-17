be34cfb28ab97281380b0adfe9291b2e1041ca72
Alex Pilon <apilon@hashicorp.com>
2018-04-17 17:16:39 -0400

    add forcenew on env var value field add acc test

--- kubernetes/schema_container.go
+++ kubernetes/schema_container.go
@@ -222,6 +222,7 @@ func containerFields(isUpdatable bool) map[string]*schema.Schema {
 					},
 					"value": {
 						Type:        schema.TypeString,
+						ForceNew:    true,
 						Optional:    true,
 						Description: `Variable references $(VAR_NAME) are expanded using the previous defined environment variables in the container and any service environment variables. If a variable cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded, regardless of whether the variable exists or not. Defaults to "".`,
 					},

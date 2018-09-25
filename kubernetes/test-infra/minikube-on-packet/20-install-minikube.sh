d4f08caca2d5fab83eceb886cfe6670c810985a4
Alex Somesan <alex.somesan@gmail.com>
2018-09-25 17:57:25 +0200

    Minikube on KVM at Packet

--- kubernetes/test-infra/minikube-on-packet/20-install-minikube.sh
+++ kubernetes/test-infra/minikube-on-packet/20-install-minikube.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/bash
+
 # Install kubectl
+
 KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl \
-  && chmod +x kubectl && mv kubectl /usr/local/bin/
+  && chmod a+x kubectl && mv kubectl /usr/local/bin/
 kubectl version
 
 # Install minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-  && chmod +x minikube && mv minikube /usr/local/bin/
+  && chmod a+x minikube && mv minikube /usr/local/bin/
 minikube version
+minikube config set WantReportErrorPrompt false
+minikube config set vm-driver kvm2

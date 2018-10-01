eea7af0dc2a1394fbda98ddf61e4ebdaa74b9ea0
Alex Somesan <alex.somesan@gmail.com>
2018-10-01 14:27:18 +0200

    Document minikube settings

--- kubernetes/test-infra/minikube-on-packet/20-install-minikube.sh
+++ kubernetes/test-infra/minikube-on-packet/20-install-minikube.sh
@@ -11,7 +11,13 @@ kubectl version
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod a+x minikube && mv minikube /usr/local/bin/
 minikube version
+
+# Setting WantReportErrorPrompt to false prevents minikube from interactively prompting the user 
+# for consent on sending diagnostic information back home in case of failure.
 minikube config set WantReportErrorPrompt false
 minikube config set vm-driver kvm2
+
+# These settings allocate half of the host's CPU cores and memory to the minikube virtual machine.
+# The proportion can be adjusted by chaning the '/ 2' factor in the expressions below.
 minikube config set cpus $(($(lscpu -p | grep -cv '#') / 2))
 minikube config set memory $(($(free --mega -tw | grep Mem: | cut -d' ' -f12) / 2))

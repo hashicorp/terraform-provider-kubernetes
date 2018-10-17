4375d7c645c219b3f83ab94c7ab3013e6303b707
Alex Somesan <alex.somesan@gmail.com>
2018-10-17 19:17:59 +0200

    Fix patch operations on resource update + other small fixes

--- kubernetes/structures_rbac.go
+++ kubernetes/structures_rbac.go
@@ -30,7 +30,7 @@ func expandRBACSubjects(in []interface{}) []api.Subject {
 	if len(in) == 0 || in[0] == nil {
 		return []api.Subject{}
 	}
-	subjects := []api.Subject{}
+	subjects := make([]api.Subject, 0, len(in))
 	for i := range in {
 		subject := api.Subject{}
 		m := in[i].(map[string]interface{})
@@ -63,7 +63,7 @@ func flattenRBACRoleRef(in api.RoleRef) interface{} {
 }
 
 func flattenRBACSubjects(in []api.Subject) []interface{} {
-	att := make([]interface{}, len(in), len(in))
+	att := make([]interface{}, 0, len(in))
 	for _, n := range in {
 		m := make(map[string]interface{})
 		if n.APIGroup != "" {
@@ -74,19 +74,39 @@ func flattenRBACSubjects(in []api.Subject) []interface{} {
 		if n.Namespace != "" {
 			m["namespace"] = n.Namespace
 		}
+		att = append(att, m)
 	}
 	return att
 }
 
 // Patch Ops
 func patchRbacSubject(d *schema.ResourceData) PatchOperations {
-	ops := make([]PatchOperation, 0, 0)
+	o, n := d.GetChange("subject")
+	oldsubjects := expandRBACSubjects(o.([]interface{}))
+	newsubjects := expandRBACSubjects(n.([]interface{}))
+	ops := make([]PatchOperation, 0, len(newsubjects)+len(oldsubjects))
 
-	if d.HasChange("subjects") {
-		subjects := expandRBACSubjects(d.Get("subjects").([]interface{}))
-		for i, v := range subjects {
-			ops = append(ops, &ReplaceOperation{
-				Path:  "/subjects/" + strconv.Itoa(i),
+	common := len(newsubjects)
+	if common > len(oldsubjects) {
+		common = len(oldsubjects)
+	}
+	for i, v := range newsubjects[:common] {
+		ops = append(ops, &ReplaceOperation{
+			Path:  "/subjects/" + strconv.Itoa(i),
+			Value: v,
+		})
+	}
+	if len(oldsubjects) > len(newsubjects) {
+		for i := len(newsubjects); i < len(oldsubjects); i++ {
+			ops = append(ops, &RemoveOperation{
+				Path: "/subjects/" + strconv.Itoa(len(oldsubjects)-i),
+			})
+		}
+	}
+	if len(newsubjects) > len(oldsubjects) {
+		for i, v := range newsubjects[common:] {
+			ops = append(ops, &AddOperation{
+				Path:  "/subjects/" + strconv.Itoa(common+i),
 				Value: v,
 			})
 		}

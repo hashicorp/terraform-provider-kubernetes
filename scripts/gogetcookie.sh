5ce889b1f592f2fb302e8c191a5dc78158b208b1
Alex Somesan <alex.somesan@gmail.com>
2019-02-05 23:13:53 +0100

    Fix github cookie for go-getting in CI

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
 touch ~/.gitcookies
 chmod 0600 ~/.gitcookies
 
 git config --global http.cookiefile ~/.gitcookies
 
 tr , \\t <<\__END__ >>~/.gitcookies
-.googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
+go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-alex.somesan.gmail.com=1/pqqFwLWAqtOsvsLsNE1FMgdyffwTGwNGvvax4yuG50k924qulFkcNacHhpDR-Kq-
+go-review.googlesource.com,FALSE,/,TRUE,2147483647,o,git-alex.somesan.gmail.com=1/pqqFwLWAqtOsvsLsNE1FMgdyffwTGwNGvvax4yuG50k924qulFkcNacHhpDR-Kq-
 __END__
+eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null

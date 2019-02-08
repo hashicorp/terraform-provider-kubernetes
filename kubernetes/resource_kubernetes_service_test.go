78d751acddff6f52775389184863065d0aa742ed
Patrick Decat <pdecat@gmail.com>
2019-02-08 13:20:59 +0100

    Add an additional test step to ensure publish_not_ready_addresses can be unset

--- kubernetes/resource_kubernetes_service_test.go
+++ kubernetes/resource_kubernetes_service_test.go
@@ -83,6 +83,35 @@ func TestAccKubernetesService_basic(t *testing.T) {
 					}),
 				),
 			},
+			{
+				Config: testAccKubernetesServiceConfig_basic(name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckKubernetesServiceExists("kubernetes_service.test", &conf),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "metadata.0.name", name),
+					resource.TestCheckResourceAttrSet("kubernetes_service.test", "metadata.0.generation"),
+					resource.TestCheckResourceAttrSet("kubernetes_service.test", "metadata.0.resource_version"),
+					resource.TestCheckResourceAttrSet("kubernetes_service.test", "metadata.0.self_link"),
+					resource.TestCheckResourceAttrSet("kubernetes_service.test", "metadata.0.uid"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.#", "1"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.port.#", "1"),
+					resource.TestCheckResourceAttrSet("kubernetes_service.test", "spec.0.cluster_ip"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.port.0.name", ""),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.port.0.node_port", "0"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.port.0.port", "8080"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.port.0.protocol", "TCP"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.port.0.target_port", "80"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.session_affinity", "None"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.type", "ClusterIP"),
+					resource.TestCheckResourceAttr("kubernetes_service.test", "spec.0.publish_not_ready_addresses", "false"),
+					testAccCheckServicePorts(&conf, []api.ServicePort{
+						{
+							Port:       int32(8080),
+							Protocol:   api.ProtocolTCP,
+							TargetPort: intstr.FromInt(80),
+						},
+					}),
+				),
+			},
 		},
 	})
 }

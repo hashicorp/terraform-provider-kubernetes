37bb1788f006f3ad8e3643ec8c828ea59ee0dc79
Patrick Decat <pdecat@gmail.com>
2019-04-17 16:33:47 +0200

    podTemplateFields: controller => owner

--- kubernetes/schema_pod_template.go
+++ kubernetes/schema_pod_template.go
@@ -6,12 +6,12 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-func podTemplateFields(controller string) map[string]*schema.Schema {
+func podTemplateFields(owner string) map[string]*schema.Schema {
 	s := map[string]*schema.Schema{
-		"metadata": metadataSchema(controller, true),
+		"metadata": metadataSchema(owner, true),
 		"spec": {
 			Type:        schema.TypeList,
-			Description: fmt.Sprintf("Spec of the pods owned by the %s", controller),
+			Description: fmt.Sprintf("Spec of the pods owned by the %s", owner),
 			Optional:    true,
 			MaxItems:    1,
 			Elem: &schema.Resource{

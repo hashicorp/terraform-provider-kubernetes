07ec290a9d17aa2b854237d765ba457b7f22e238
Alex Somesan <alex.somesan@gmail.com>
2019-07-23 18:40:54 +0200

    Migrate GKE test infra to 0.12

--- kubernetes/test-infra/gke/main.tf
+++ kubernetes/test-infra/gke/main.tf
@@ -2,7 +2,8 @@ provider "google" {
   // Provider settings to be provided via ENV variables
 }
 
-data "google_compute_zones" "available" {}
+data "google_compute_zones" "available" {
+}
 
 resource "random_id" "cluster_name" {
   byte_length = 10
@@ -25,24 +26,24 @@ variable "workers_count" {
 }
 
 data "google_container_engine_versions" "supported" {
-  zone           = "${data.google_compute_zones.available.names[0]}"
-  version_prefix = "${var.kubernetes_version}"
+  zone           = data.google_compute_zones.available.names[0]
+  version_prefix = var.kubernetes_version
 }
 
 resource "google_container_cluster" "primary" {
   name               = "tf-acc-test-${random_id.cluster_name.hex}"
-  zone               = "${data.google_compute_zones.available.names[0]}"
-  initial_node_count = "${var.workers_count}"
-  node_version       = "${data.google_container_engine_versions.supported.latest_node_version}"
-  min_master_version = "${data.google_container_engine_versions.supported.latest_master_version}"
+  zone               = data.google_compute_zones.available.names[0]
+  initial_node_count = var.workers_count
+  node_version       = data.google_container_engine_versions.supported.latest_node_version
+  min_master_version = data.google_container_engine_versions.supported.latest_master_version
 
   additional_zones = [
-    "${data.google_compute_zones.available.names[1]}",
+    data.google_compute_zones.available.names[1],
   ]
 
   master_auth {
-    username = "${random_id.username.hex}"
-    password = "${random_id.password.hex}"
+    username = random_id.username.hex
+    password = random_id.password.hex
   }
 
   node_config {
@@ -58,32 +59,33 @@ resource "google_container_cluster" "primary" {
 }
 
 data "template_file" "kubeconfig" {
-  template = "${file("${path.module}/kubeconfig-template.yaml")}"
+  template = file("${path.module}/kubeconfig-template.yaml")
 
-  vars {
-    cluster_name    = "${google_container_cluster.primary.name}"
-    user_name       = "${google_container_cluster.primary.master_auth.0.username}"
-    user_password   = "${google_container_cluster.primary.master_auth.0.password}"
-    endpoint        = "${google_container_cluster.primary.endpoint}"
-    cluster_ca      = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-    client_cert     = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-    client_cert_key = "${google_container_cluster.primary.master_auth.0.client_key}"
+  vars = {
+    cluster_name    = google_container_cluster.primary.name
+    user_name       = google_container_cluster.primary.master_auth[0].username
+    user_password   = google_container_cluster.primary.master_auth[0].password
+    endpoint        = google_container_cluster.primary.endpoint
+    cluster_ca      = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+    client_cert     = google_container_cluster.primary.master_auth[0].client_certificate
+    client_cert_key = google_container_cluster.primary.master_auth[0].client_key
   }
 }
 
 resource "local_file" "kubeconfig" {
-  content  = "${data.template_file.kubeconfig.rendered}"
+  content  = data.template_file.kubeconfig.rendered
   filename = "${path.module}/kubeconfig"
 }
 
 output "google_zone" {
-  value = "${data.google_compute_zones.available.names[0]}"
+  value = data.google_compute_zones.available.names[0]
 }
 
 output "node_version" {
-  value = "${google_container_cluster.primary.node_version}"
+  value = google_container_cluster.primary.node_version
 }
 
 output "kubeconfig_path" {
-  value = "${local_file.kubeconfig.filename}"
+  value = local_file.kubeconfig.filename
 }
+

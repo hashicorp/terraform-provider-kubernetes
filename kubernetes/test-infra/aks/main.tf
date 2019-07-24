8b0366d69f0817968cfa645aca035c706641de6b
Alex Somesan <alex.somesan@gmail.com>
2019-07-24 12:48:06 +0200

    Migrate AKS test infra to 0.12

--- kubernetes/test-infra/aks/main.tf
+++ kubernetes/test-infra/aks/main.tf
@@ -1,13 +1,14 @@
 locals {
   random_prefix = "${var.prefix}-${random_id.tf-k8s-acc.hex}"
 }
+
 provider "azurerm" {
   version = "~> 1.28.0"
 }
 
 data "azurerm_kubernetes_service_versions" "current" {
-  location       = "${var.location}"
-  version_prefix = "${var.kubernetes_version}"
+  location       = var.location
+  version_prefix = var.kubernetes_version
 }
 
 resource "random_id" "tf-k8s-acc" {
@@ -16,13 +17,13 @@ resource "random_id" "tf-k8s-acc" {
 
 resource "azurerm_resource_group" "tf-k8s-acc" {
   name     = "${local.random_prefix}-rsg"
-  location = "${var.location}"
+  location = var.location
 }
 
 resource "azurerm_route_table" "tf-k8s-acc" {
   name                = "${local.random_prefix}-rt"
-  location            = "${azurerm_resource_group.tf-k8s-acc.location}"
-  resource_group_name = "${azurerm_resource_group.tf-k8s-acc.name}"
+  location            = azurerm_resource_group.tf-k8s-acc.location
+  resource_group_name = azurerm_resource_group.tf-k8s-acc.name
 
   route {
     name                   = "default"
@@ -34,32 +35,32 @@ resource "azurerm_route_table" "tf-k8s-acc" {
 
 resource "azurerm_virtual_network" "tf-k8s-acc" {
   name                = "${local.random_prefix}-network"
-  location            = "${azurerm_resource_group.tf-k8s-acc.location}"
-  resource_group_name = "${azurerm_resource_group.tf-k8s-acc.name}"
+  location            = azurerm_resource_group.tf-k8s-acc.location
+  resource_group_name = azurerm_resource_group.tf-k8s-acc.name
   address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "tf-k8s-acc" {
   name                 = "${local.random_prefix}-internal"
-  resource_group_name  = "${azurerm_resource_group.tf-k8s-acc.name}"
+  resource_group_name  = azurerm_resource_group.tf-k8s-acc.name
   address_prefix       = "10.1.0.0/24"
-  virtual_network_name = "${azurerm_virtual_network.tf-k8s-acc.name}"
+  virtual_network_name = azurerm_virtual_network.tf-k8s-acc.name
 
   # this field is deprecated and will be removed in 2.0 - but is required until then
-  route_table_id = "${azurerm_route_table.tf-k8s-acc.id}"
+  route_table_id = azurerm_route_table.tf-k8s-acc.id
 }
 
 resource "azurerm_subnet_route_table_association" "tf-k8s-acc" {
-  subnet_id      = "${azurerm_subnet.tf-k8s-acc.id}"
-  route_table_id = "${azurerm_route_table.tf-k8s-acc.id}"
+  subnet_id      = azurerm_subnet.tf-k8s-acc.id
+  route_table_id = azurerm_route_table.tf-k8s-acc.id
 }
 
 resource "azurerm_kubernetes_cluster" "tf-k8s-acc" {
   name                = "${local.random_prefix}-cluster"
-  resource_group_name = "${azurerm_resource_group.tf-k8s-acc.name}"
-  location            = "${azurerm_resource_group.tf-k8s-acc.location}"
+  resource_group_name = azurerm_resource_group.tf-k8s-acc.name
+  location            = azurerm_resource_group.tf-k8s-acc.location
   dns_prefix          = "${local.random_prefix}-cluster"
-  kubernetes_version  = "${data.azurerm_kubernetes_service_versions.current.latest_version}"
+  kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
 
   # Uncomment to enable SSH access to nodes
   #
@@ -72,18 +73,18 @@ resource "azurerm_kubernetes_cluster" "tf-k8s-acc" {
 
   agent_pool_profile {
     name            = "agentpool"
-    count           = "${var.workers_count}"
-    vm_size         = "${var.workers_type}"
+    count           = var.workers_count
+    vm_size         = var.workers_type
     os_type         = "Linux"
     os_disk_size_gb = 30
 
     # Required for advanced networking
-    vnet_subnet_id = "${azurerm_subnet.tf-k8s-acc.id}"
+    vnet_subnet_id = azurerm_subnet.tf-k8s-acc.id
   }
 
   service_principal {
-    client_id     = "${var.aks_client_id}"
-    client_secret = "${var.aks_client_secret}"
+    client_id     = var.aks_client_id
+    client_secret = var.aks_client_secret
   }
 
   role_based_access_control {
@@ -96,6 +97,7 @@ resource "azurerm_kubernetes_cluster" "tf-k8s-acc" {
 }
 
 resource "local_file" "kubeconfig" {
-  content  = "${azurerm_kubernetes_cluster.tf-k8s-acc.kube_config_raw}"
+  content  = azurerm_kubernetes_cluster.tf-k8s-acc.kube_config_raw
   filename = "${path.module}/kubeconfig"
 }
+
--- kubernetes/test-infra/aks/outputs.tf
+++ kubernetes/test-infra/aks/outputs.tf
@@ -1,3 +1,4 @@
 output "kubeconfig_path" {
-  value = "${local_file.kubeconfig.filename}"
+  value = local_file.kubeconfig.filename
 }
+

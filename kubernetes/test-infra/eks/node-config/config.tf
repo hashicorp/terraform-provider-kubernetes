08de38374cf2a516695cc8b65454d752d108af93
Alex Somesan <alex.somesan@gmail.com>
2019-07-25 22:15:32 +0200

    More EKS corrections

--- kubernetes/test-infra/eks/node-config/config.tf
+++ kubernetes/test-infra/eks/node-config/config.tf
@@ -21,17 +21,20 @@ MAPPEDROLE
 
 resource "local_file" "cluster_ca" {
   content = base64decode(var.cluster_ca)
-  filename = "${path.module}/cluster_ca"
+  filename = "${path.root}/cluster_ca"
 }
 
 resource "null_resource" "wait_for_api" {
   depends_on = [local_file.cluster_ca]
 
   provisioner "local-exec" {
-
-    working_dir = path.module
+    working_dir = path.root
+    environment = {
+      K8S_CA_FILE = abspath(local_file.cluster_ca.filename)
+      K8S_ENDPOINT = var.cluster_endpoint
+    }
     command = <<CMDEOF
-while ! curl -s --cacert ./cluster_ca ${var.cluster_endpoint}/version > /dev/null; do 
+while ! curl -s --cacert $K8S_CA_FILE $K8S_ENDPOINT/version; do 
   echo "Waiting for the cluster API to come online..."
   sleep 3
 done

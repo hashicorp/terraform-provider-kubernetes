fa8965a32cc7e1ff115f75b117ac97495f83aea8
Andrew Royle <andrew.x.royle@gsk.com>
2019-09-17 16:59:45 +0100

    Add resource_kubernetes_pod_security_policy

--- kubernetes/provider.go
+++ kubernetes/provider.go
@@ -158,6 +158,7 @@ func Provider() terraform.ResourceProvider {
 			"kubernetes_persistent_volume":         resourceKubernetesPersistentVolume(),
 			"kubernetes_persistent_volume_claim":   resourceKubernetesPersistentVolumeClaim(),
 			"kubernetes_pod":                       resourceKubernetesPod(),
+			"kubernetes_pod_security_policy":       resourceKubernetesPodSecurityPolicy(),
 			"kubernetes_priority_class":            resourceKubernetesPriorityClass(),
 			"kubernetes_replication_controller":    resourceKubernetesReplicationController(),
 			"kubernetes_role_binding":              resourceKubernetesRoleBinding(),

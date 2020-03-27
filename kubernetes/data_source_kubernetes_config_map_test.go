6fd35b2b5ac0887d8ee974b4b42eb25f9a8399de
Stef Forrester <sforrester@hashicorp.com>
2020-03-27 14:17:53 -0700

    Add data source for config map
    Add data source for reading config maps.
    Based on PR https://github.com/terraform-providers/terraform-provider-kubernetes/pull/90.
    Solves https://github.com/terraform-providers/terraform-provider-kubernetes/issues/76.

---
+++ kubernetes/data_source_kubernetes_config_map_test.go
@@ -0,0 +1,49 @@
+package kubernetes
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccKubernetesDataSourceConfigMap_basic(t *testing.T) {
+	name := fmt.Sprintf("tf-acc-test-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccKubernetesDataSourceConfigMapConfig_basic(name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "metadata.0.name", name),
+					resource.TestCheckResourceAttrSet("data.kubernetes_config_map.test", "metadata.0.generation"),
+					resource.TestCheckResourceAttrSet("data.kubernetes_config_map.test", "metadata.0.resource_version"),
+					resource.TestCheckResourceAttrSet("data.kubernetes_config_map.test", "metadata.0.self_link"),
+					resource.TestCheckResourceAttrSet("data.kubernetes_config_map.test", "metadata.0.uid"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "metadata.0.annotations.%", "2"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "metadata.0.annotations.TestAnnotationOne", "one"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "metadata.0.annotations.TestAnnotationTwo", "two"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "metadata.0.labels.TestLabelOne", "one"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "metadata.0.labels.TestLabelTwo", "two"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "metadata.0.labels.TestLabelThree", "three"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "data.%", "2"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "data.one", "first"),
+					resource.TestCheckResourceAttr("data.kubernetes_config_map.test", "data.two", "second"),
+				),
+			},
+		},
+	})
+}
+
+func testAccKubernetesDataSourceConfigMapConfig_basic(name string) string {
+	return testAccKubernetesConfigMapConfig_basic(name) + `
+data "kubernetes_config_map" "test" {
+	metadata {
+		name = "${kubernetes_config_map.test.metadata.0.name}"
+	}
+}
+`
+}
--- kubernetes/provider.go
+++ kubernetes/provider.go
@@ -137,6 +137,7 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{
+			"kubernetes_config_map":    dataSourceKubernetesConfigMap(),
 			"kubernetes_secret":        dataSourceKubernetesSecret(),
 			"kubernetes_service":       dataSourceKubernetesService(),
 			"kubernetes_storage_class": dataSourceKubernetesStorageClass(),

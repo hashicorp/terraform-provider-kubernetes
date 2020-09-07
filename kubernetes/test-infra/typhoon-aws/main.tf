91df1b170533caa7ef308e9f9460bf22119d0826
Alex Somesan <alex.somesan@gmail.com>
2020-09-08 01:50:22 +0200

    Refactor Typhoon test configuration to allow selection between 1.18.x and 1.19.x versions of Kubernetes.

--- kubernetes/test-infra/typhoon-aws/main.tf
+++ kubernetes/test-infra/typhoon-aws/main.tf
@@ -26,28 +26,3 @@ resource "null_resource" "ssh-key" {
 data "aws_route53_zone" "typhoon-acc" {
   name = var.base_domain
 }
-
-module "typhoon-acc" {
-  source = "git::https://github.com/poseidon/typhoon//aws/fedora-coreos/kubernetes?ref=v1.18.0" # set the desired Kubernetes version here
-
-  cluster_name = var.cluster_name
-  dns_zone     = var.base_domain
-  dns_zone_id  = data.aws_route53_zone.typhoon-acc.zone_id
-
-  # node configuration
-  ssh_authorized_key = tls_private_key.typhoon-acc.public_key_openssh
-
-  worker_count = var.worker_count
-  controller_count = var.controller_count
-  worker_type  = var.controller_type
-  controller_type = var.worker_type
-}
-
-resource "local_file" "typhoon-acc" {
-  content  = module.typhoon-acc.kubeconfig-admin
-  filename = "kubeconfig"
-}
-
-output "kubeconfig_path" {
-  value = "${path.cwd}/${local_file.typhoon-acc.filename}"
-}

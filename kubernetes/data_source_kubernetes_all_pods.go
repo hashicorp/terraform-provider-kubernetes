598b24b801405dd605f2332ee74aada266e91ec3
jbarnes <jbarnes7952@gmail.com>
2020-09-13 00:55:09 -0400

    all_pods

---
+++ kubernetes/data_source_kubernetes_all_pods.go
@@ -0,0 +1,59 @@
+package kubernetes
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func dataSourceKubernetesAllPods() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceKubernetesAllPodsRead,
+		Schema: map[string]*schema.Schema{
+			"pods": {
+				Type:        schema.TypeList,
+				Description: "List of all pods in a cluster.",
+				Computed:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+	}
+}
+
+func dataSourceKubernetesAllPodsRead(d *schema.ResourceData, meta interface{}) error {
+	conn, err := meta.(KubeClientsets).MainClientset()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Listing pods")
+	nsRaw, err := conn.CoreV1().Pods().List(metav1.ListOptions{})
+	if err != nil {
+		log.Printf("[DEBUG] Received error: %#v", err)
+		return err
+	}
+	pods := make([]string, len(nsRaw.Items))
+	for i, v := range nsRaw.Items {
+		pods[i] = string(v.Name)
+	}
+	log.Printf("[INFO] Received pods: %#v", pods)
+	err = d.Set("pods", pods)
+	if err != nil {
+		return err
+	}
+	idsum := sha256.New()
+	for _, v := range pods {
+		_, err := idsum.Write([]byte(v))
+		if err != nil {
+			return err
+		}
+	}
+	id := fmt.Sprintf("%x", idsum.Sum(nil))
+	d.SetId(id)
+	return nil
+}
---
+++ kubernetes/data_source_kubernetes_all_pods_test.go
@@ -0,0 +1,33 @@
+package kubernetes
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccKubernetesDataSourceAllPods_basic(t *testing.T) {
+	rxPosNum := regexp.MustCompile("^[1-9][0-9]*$")
+	nsName := regexp.MustCompile("^[a-zA-Z][-\\w]*$")
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccKubernetesDataSourceAllPodsConfig_basic(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("data.kubernetes_all_pods.test", "pods.#", rxPosNum),
+					resource.TestCheckResourceAttrSet("data.kubernetes_all_pods.test", "pods.0"),
+					resource.TestMatchResourceAttr("data.kubernetes_all_pods.test", "pods.0", nsName),
+				),
+			},
+		},
+	})
+}
+
+func testAccKubernetesDataSourceAllPodsConfig_basic() string {
+	return `
+data "kubernetes_all_pods" "test" {}
+`
+}

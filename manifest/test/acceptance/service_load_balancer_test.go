6cfae1930c8aae3743e3aaf20bc7a428bda4e57b
Phil Sautter <psautter@hashicorp.com>
2021-04-29 21:00:21 -0700

    Add service acceptance tests

---
+++ manifest/test/acceptance/service_load_balancer_test.go
@@ -0,0 +1,76 @@
+// +build acceptance
+
+package acceptance
+
+import (
+	"encoding/json"
+	"testing"
+
+	tfstatehelper "github.com/hashicorp/terraform-provider-kubernetes-alpha/test/helper/state"
+)
+
+// This test case tests a Service but also is a demonstration of some the assert functions
+// available in the test helper
+func TestKubernetesManifest_Service_LoadBalancer(t *testing.T) {
+	name := randName()
+	namespace := randName()
+
+	tf := tfhelper.RequireNewWorkingDir(t)
+	tf.SetReattachInfo(reattachInfo)
+	defer func() {
+		tf.RequireDestroy(t)
+		tf.Close()
+		k8shelper.AssertNamespacedResourceDoesNotExist(t, "v1", "services", namespace, name)
+	}()
+
+	k8shelper.CreateNamespace(t, namespace)
+	defer k8shelper.DeleteNamespace(t, namespace)
+
+	tfvars := TFVARS{
+		"namespace": namespace,
+		"name":      name,
+	}
+	tfconfig := loadTerraformConfig(t, "Service_LoadBalancer/service.tf", tfvars)
+	tf.RequireSetConfig(t, tfconfig)
+	tf.RequireInit(t)
+	tf.RequireApply(t)
+
+	k8shelper.AssertNamespacedResourceExists(t, "v1", "services", namespace, name)
+
+	tfstate := tfstatehelper.NewHelper(tf.RequireState(t))
+	tfstate.AssertAttributeValues(t, tfstatehelper.AttributeValues{
+		"kubernetes_manifest.test.object.metadata.namespace":      namespace,
+		"kubernetes_manifest.test.object.metadata.name":           name,
+		"kubernetes_manifest.test.object.spec.ports.0.name":       "http",
+		"kubernetes_manifest.test.object.spec.ports.0.port":       json.Number("80"),
+		"kubernetes_manifest.test.object.spec.ports.0.targetPort": json.Number("8080"),
+		"kubernetes_manifest.test.object.spec.ports.0.protocol":   "TCP",
+		"kubernetes_manifest.test.object.spec.selector.app":       "test",
+		"kubernetes_manifest.test.object.spec.type":               "LoadBalancer",
+	})
+
+	tfconfigModified := loadTerraformConfig(t, "Service_LoadBalancer/service_modified.tf", tfvars)
+	tf.RequireSetConfig(t, tfconfigModified)
+	tf.RequireApply(t)
+
+	tfstate = tfstatehelper.NewHelper(tf.RequireState(t))
+	tfstate.AssertAttributeValues(t, tfstatehelper.AttributeValues{
+		"kubernetes_manifest.test.object.metadata.namespace":        namespace,
+		"kubernetes_manifest.test.object.metadata.name":             name,
+		"kubernetes_manifest.test.object.metadata.annotations.test": "1",
+		"kubernetes_manifest.test.object.metadata.labels.test":      "2",
+		"kubernetes_manifest.test.object.spec.ports.0.name":         "https",
+		"kubernetes_manifest.test.object.spec.ports.0.port":         json.Number("443"),
+		"kubernetes_manifest.test.object.spec.ports.0.targetPort":   json.Number("8443"),
+		"kubernetes_manifest.test.object.spec.ports.0.protocol":     "TCP",
+		"kubernetes_manifest.test.object.spec.selector.app":         "test",
+		"kubernetes_manifest.test.object.spec.type":                 "LoadBalancer",
+	})
+
+	tfstate.AssertAttributeLen(t, "kubernetes_manifest.test.object.metadata.labels", 1)
+	tfstate.AssertAttributeLen(t, "kubernetes_manifest.test.object.metadata.annotations", 1)
+
+	tfstate.AssertAttributeNotEmpty(t, "kubernetes_manifest.test.object.metadata.labels.test")
+
+	tfstate.AssertAttributeDoesNotExist(t, "kubernetes_manifest.test.spec")
+}
---
+++ manifest/test/acceptance/testdata/Service_ClusterIP/service_modified.tf
@@ -0,0 +1,34 @@
+provider "kubernetes-alpha" {
+}
+
+resource "kubernetes_manifest" "test" {
+  provider = kubernetes-alpha
+
+  manifest = {
+    apiVersion = "v1"
+    kind       = "Service"
+    metadata = {
+      name      = var.name
+      namespace = var.namespace
+      annotations = {
+        test = "1"
+      }
+      labels = {
+        test = "2"
+      }
+    }
+    spec = {
+      ports = [{
+        name       = "https",
+        port       = 443,
+        targetPort = 8443,
+        # Protcol is required for serverside apply per https://github.com/kubernetes-sigs/structured-merge-diff/issues/130
+        protocol = "TCP"
+      }]
+      selector = {
+        app = "test"
+      }
+      type = "ClusterIP"
+    }
+  }
+}

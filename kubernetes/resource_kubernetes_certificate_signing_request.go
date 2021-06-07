62d0df7a996b0a0b7c4065faf72ad2581d299c80
Stef Forrester <sforrester@hashicorp.com>
2021-06-07 09:06:34 -0700

    fix lint errors

--- kubernetes/resource_kubernetes_certificate_signing_request.go
+++ kubernetes/resource_kubernetes_certificate_signing_request.go
@@ -128,7 +128,7 @@ func resourceKubernetesCertificateSigningRequestCreate(ctx context.Context, d *s
 			return updateErr
 		})
 		if retryErr != nil {
-			panic(diag.Errorf("CSR auto-approve update failed: %v", retryErr))
+			diag.Errorf("CSR auto-approve update failed: %v", retryErr)
 		}
 		fmt.Println("CSR auto-approve update succeeded")
 	}

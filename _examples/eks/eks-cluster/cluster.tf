e6a372ee16b58798cf2d6b8fcae30b36904db71c
Stef Forrester <sforrester@hashicorp.com>
2021-06-10 09:45:19 -0700

    increase nodes since I ran out during testing

--- _examples/eks/eks-cluster/cluster.tf
+++ _examples/eks/eks-cluster/cluster.tf
@@ -22,8 +22,8 @@ resource "aws_eks_node_group" "k8s-acc" {
   subnet_ids      = aws_subnet.k8s-acc.*.id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 

7c369cd69b9d130085b062b9fc162bdc54fdc371
John Houston <jhouston@hashicorp.com>
2021-07-06 00:54:56 -0400

    Remove superfluous comments from main.go

--- main.go
+++ main.go
@@ -1,27 +1,3 @@
-// package main
-
-// import (
-// 	"context"
-// 	"flag"
-
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-// 	"github.com/hashicorp/terraform-provider-kubernetes/kubernetes"
-// )
-
-// func main() {
-// 	debugFlag := flag.Bool("debug", false, "Start provider in stand-alone debug mode.")
-// 	flag.Parse()
-
-// 	serveOpts := &plugin.ServeOpts{
-// 		ProviderFunc: kubernetes.Provider,
-// 	}
-// 	if debugFlag != nil && *debugFlag {
-// 		plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/kubernetes", serveOpts)
-// 	} else {
-// 		plugin.Serve(serveOpts)
-// 	}
-// }
-
 package main
 
 import (

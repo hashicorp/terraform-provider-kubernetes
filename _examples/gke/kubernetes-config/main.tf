bdf92e0c6623bc8130b05d66827794998b7ce640
Stef Forrester <sforrester@hashicorp.com>
2021-07-22 12:53:13 -0700

    working GKE example

--- _examples/gke/kubernetes-config/main.tf
+++ _examples/gke/kubernetes-config/main.tf
@@ -1,3 +1,5 @@
+# Configure kubernetes provider with Oauth2 access token.
+# This fetches a new token, which will expire in 1 hour.
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
 data "google_client_config" "default" {
 }
@@ -6,38 +8,24 @@ data "google_container_cluster" "default" {
   name = var.cluster_name
 }
 
-#provider "kubernetes" {
-#  host                   = "https://${data.google_container_cluster.default.endpoint}"
-#  cluster_ca_certificate = base64decode(
-#    data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
-#  )
-#  exec {
-#    command     = "gcloud"
-#    api_version = "client.authentication.k8s.io/v1alpha1"
-#    args        = ["container", "clusters", "get-credentials", var.cluster_name,
-#                   "--zone", data.google_client_config.default.zone, "--project",
-#                   data.google_client_config.default.project
-#                  ]
-#  }
-#}
-#
-#provider "helm" {
-#  kubernetes {
-#    host                   = "https://${data.google_container_cluster.default.endpoint}"
-#    cluster_ca_certificate = base64decode(
-#      data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
-#    )
-#    exec {
-#      command     = "gcloud"
-#      api_version = "client.authentication.k8s.io/v1alpha1"
-#      args        = ["container", "clusters", "get-credentials", var.cluster_name, "--zone",
-#                     data.google_client_config.default.zone, "project",
-#                     data.google_client_config.default.project
-#                    ]
-#    }
-#  }
-#}
-#
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.default.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.default.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+    )
+  }
+}
+
 resource "local_file" "kubeconfig" {
   filename          = "./kubeconfig"
   sensitive_content = templatefile("${path.module}/kubeconfig.tpl", {
@@ -49,20 +37,20 @@ resource "local_file" "kubeconfig" {
   })
 }
 
-#resource "kubernetes_namespace" "test" {
-#  metadata {
-#    name = "test"
-#  }
-#}
-#
-#resource "helm_release" "nginx_ingress" {
-#  namespace  = kubernetes_namespace.test.metadata.0.name
-#  wait       = true
-#  timeout    = 600
-#
-#  name       = "ingress-nginx"
-#
-#  repository = "https://kubernetes.github.io/ingress-nginx"
-#  chart      = "ingress-nginx"
-#  version    = "v3.30.0"
-#}
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "test"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  namespace  = kubernetes_namespace.test.metadata.0.name
+  wait       = true
+  timeout    = 600
+
+  name       = "ingress-nginx"
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "v3.30.0"
+}

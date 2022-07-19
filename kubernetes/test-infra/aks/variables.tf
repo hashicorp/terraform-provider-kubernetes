6a71f78784e4a57eab1efbe23406bf9f2a702b37
Alex Somesan <alex.somesan@gmail.com>
2022-07-19 19:00:55 +0200

    Adapt AKS infra for managed identity rather than explicit service account creds

--- kubernetes/test-infra/aks/variables.tf
+++ kubernetes/test-infra/aks/variables.tf
@@ -22,14 +22,6 @@ variable "workers_type" {
   default = "Standard_DS4_v2"
 }
 
-variable "aks_client_id" {
-  description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster"
-}
-
-variable "aks_client_secret" {
-  description = "The Client Secret for the Service Principal to use for this Managed Kubernetes Cluster"
-}
-
 # Uncomment to enable SSH access to nodes
 #
 # variable "public_ssh_key_path" {

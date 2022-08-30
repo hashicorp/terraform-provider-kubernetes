3a4107396251422306973590af24cff027d3e2e1
Alex Somesan <alex.somesan@gmail.com>
2022-08-30 16:27:21 +0200

    [TK-1382] Upgrade SDKs (#1798)
    * Upgrade terraform-plugin-go to v0.13.0
    
    * Upgrade terraform-plugin-sdk to v2.20.0
    
    * Upgrade terraform-plugin-mux to v0.7.0
    
    * Copy updated 'plugintest' and 'logging' packages from terraform-plugin-sdk module
    
    * Re-vendor github.com/hashicorp/terraform-plugin-log after fixing dependent import paths
    
    * Update webhook setup for kubernetes_manifest ComputedFields acceptance test.
    
    * Update checks on TestAccKubernetesStatefulSet_Update
    
    * Update checks on TestAccKubernetesNetworkPolicy_basic
    
    * Update checks on TestAccKubernetesPersistentVolume_hostPath_nodeAffinity
    
    * Bump versions yet again

--- vendor/github.com/hashicorp/terraform-plugin-mux/tf5muxserver/mux_server_PrepareProviderConfig.go
+++ vendor/github.com/hashicorp/terraform-plugin-mux/tf5muxserver/mux_server_PrepareProviderConfig.go
@@ -48,7 +48,7 @@ func (s muxServer) PrepareProviderConfig(ctx context.Context, req *tfprotov5.Pre
 			continue
 		}
 
-		equal, err := dynamicValueEquals(schemaType(s.providerSchema), res.PreparedConfig, resp.PreparedConfig)
+		equal, err := dynamicValueEquals(s.providerSchema.ValueType(), res.PreparedConfig, resp.PreparedConfig)
 
 		if err != nil {
 			return nil, fmt.Errorf("unable to compare PrepareProviderConfig PreparedConfig responses: %w", err)

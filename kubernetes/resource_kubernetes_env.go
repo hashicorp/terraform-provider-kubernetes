af4c578283162c0858c710fe55d790bc384b0fc6
BBBmau <alvarez.mauriciotm@gmail.com>
2022-09-28 16:11:37 -0700

    add value_from to schema

--- kubernetes/resource_kubernetes_env.go
+++ kubernetes/resource_kubernetes_env.go
@@ -58,7 +58,7 @@ func resourceKubernetesEnv() *schema.Resource {
 			},
 			"container": {
 				Type:        schema.TypeString,
-				Description: "",
+				Description: "Name of the container for which we are updating the environment variables.",
 				Required:    true,
 			},
 			"api_version": {
@@ -73,7 +73,7 @@ func resourceKubernetesEnv() *schema.Resource {
 			},
 			"env": {
 				Type:        schema.TypeList,
-				Description: "Rule defining a set of permissions for the role",
+				Description: "List of custom values used to represent environment variables",
 				Required:    true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -87,6 +87,113 @@ func resourceKubernetesEnv() *schema.Resource {
 							Optional:    true,
 							Description: `Variable references $(VAR_NAME) are expanded using the previous defined environment variables in the container and any service environment variables. If a variable cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded, regardless of whether the variable exists or not. Defaults to "".`,
 						},
+						"value_from": {
+							Type:        schema.TypeList,
+							Optional:    true,
+							MaxItems:    1,
+							Description: "Source for the environment variable's value",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"config_map_key_ref": {
+										Type:        schema.TypeList,
+										Optional:    true,
+										MaxItems:    1,
+										Description: "Selects a key of a ConfigMap.",
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"key": {
+													Type:        schema.TypeString,
+													Optional:    true,
+													Description: "The key to select.",
+												},
+												"name": {
+													Type:        schema.TypeString,
+													Optional:    true,
+													Description: "Name of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+												},
+												"optional": {
+													Type:        schema.TypeBool,
+													Optional:    true,
+													Description: "Specify whether the ConfigMap or its key must be defined.",
+												},
+											},
+										},
+									},
+									"field_ref": {
+										Type:        schema.TypeList,
+										Optional:    true,
+										MaxItems:    1,
+										Description: "Selects a field of the pod: supports metadata.name, metadata.namespace, metadata.labels, metadata.annotations, spec.nodeName, spec.serviceAccountName, status.podIP.",
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"api_version": {
+													Type:        schema.TypeString,
+													Optional:    true,
+													Default:     "v1",
+													Description: `Version of the schema the FieldPath is written in terms of, defaults to "v1".`,
+												},
+												"field_path": {
+													Type:        schema.TypeString,
+													Optional:    true,
+													Description: "Path of the field to select in the specified API version",
+												},
+											},
+										},
+									},
+									"resource_field_ref": {
+										Type:        schema.TypeList,
+										Optional:    true,
+										MaxItems:    1,
+										Description: "Selects a resource of the container: only resources limits and requests (limits.cpu, limits.memory, limits.ephemeral-storage, requests.cpu, requests.memory and requests.ephemeral-storage) are currently supported.",
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"container_name": {
+													Type:     schema.TypeString,
+													Optional: true,
+												},
+												"divisor": {
+													Type:             schema.TypeString,
+													Optional:         true,
+													Default:          "1",
+													ValidateFunc:     validateResourceQuantity,
+													DiffSuppressFunc: suppressEquivalentResourceQuantity,
+												},
+												"resource": {
+													Type:        schema.TypeString,
+													Required:    true,
+													Description: "Resource to select",
+												},
+											},
+										},
+									},
+									"secret_key_ref": {
+										Type:        schema.TypeList,
+										Optional:    true,
+										MaxItems:    1,
+										Description: "Selects a key of a secret in the pod's namespace.",
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"key": {
+													Type:        schema.TypeString,
+													Optional:    true,
+													Description: "The key of the secret to select from. Must be a valid secret key.",
+												},
+												"name": {
+													Type:        schema.TypeString,
+													Optional:    true,
+													Description: "Name of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+												},
+												"optional": {
+													Type:        schema.TypeBool,
+													Optional:    true,
+													Description: "Specify whether the Secret or its key must be defined.",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
 					},
 				},
 			},
@@ -97,7 +204,7 @@ func resourceKubernetesEnv() *schema.Resource {
 			},
 			"field_manager": {
 				Type:        schema.TypeString,
-				Description: "Set the name of the field manager for the specified labels.",
+				Description: "Set the name of the field manager for the specified environment variables.",
 				Optional:    true,
 				Default:     defaultFieldManagerName,
 			},

cb653523da3c5d5d58e5713ab51e6dc0c04a1708
Alex Somesan <alex.somesan@gmail.com>
2022-10-07 16:24:18 +0200

    Enable AWS test infra to scale the number of AZs

--- kubernetes/test-infra/eks/variables.tf
+++ kubernetes/test-infra/eks/variables.tf
@@ -1,15 +1,21 @@
 variable "cluster_version" {
-  default = "1.21"
+  default = "1.23"
 }
 
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "node_count" {
-  default = 2
+variable "nodes_per_az" {
+  default = 1
+  type    = number
 }
 
 variable "instance_type" {
   default = "m5.large"
+}
+
+variable "az_span" {
+  type    = number
+  default = 3
+  validation {
+    condition     = var.az_span > 1
+    error_message = "Cluster must span at least 2 AZs"
+  }
 }

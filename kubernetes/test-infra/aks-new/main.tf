463791be13d76f2ac65e62f840344ded5f38e38d
sheneska.williams <sheneska.williams@hashicorp.com>
2023-03-03 13:04:07 -0500

    WIP

--- kubernetes/test-infra/aks-new/main.tf
+++ kubernetes/test-infra/aks-new/main.tf
@@ -2,29 +2,45 @@ provider "azurerm" {
   features {}
 }
 
+variable "location" {
+  type = string
+  default = "West Europe"
+}
 
-# TODO:
-# Generate a random name for the aks cluster
-# Make variables for:
-# - location
-# - node count
-# - vm size
+variable "node_count" {
+  type = number
+  default = 2
+}
+
+variable "vm_size" {
+  type = string
+  default = "Standard_A4_v2"
+}
+
+variable "kubernetes_version" {
+  type = string
+  
+}
+
+resource "random_pet" "name" {}
 
 resource "azurerm_resource_group" "test_group" {
-  name     = "test-group"
-  location = "West Europe"
+  name     = "test-aks-${random_pet.name.id}"
+  location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "test" {
-  name                = "test-aks1"
+  name                = "test-aks-${random_pet.name.id}"
   location            = azurerm_resource_group.test_group.location
   resource_group_name = azurerm_resource_group.test_group.name
   dns_prefix          = "test"
+  kubernetes_version  = var.kubernetes_version
 
   default_node_pool {
     name       = "default"
-    node_count = 2
-    vm_size    = "Standard_A4_v2"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+    //vm_size    = "Standard_A4_v2"
   }
 
   identity {
@@ -32,9 +48,18 @@ resource "azurerm_kubernetes_cluster" "test" {
   }
 }
 
-output "kube_config" {
+resource "local_file" "kubeconfig" {
+  content  = azurerm_kubernetes_cluster.test.kube_config_raw
+  filename = "${path.module}/kubeconfig"
+}
+
+output "kubeconfig" {
   value = azurerm_kubernetes_cluster.test.kube_config_raw
 
   sensitive = true
+}
+
+output "cluster_name" {
+  value = "test-aks-${random_pet.name.id}"
 }
 

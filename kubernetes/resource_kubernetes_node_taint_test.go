99f161bfccfd69c01b569b57fb269e42456de2a9
BBBmau <alvarez.mauriciotm@gmail.com>
2023-03-31 14:09:22 -0700

    add tests for multiple taints

--- kubernetes/resource_kubernetes_node_taint_test.go
+++ kubernetes/resource_kubernetes_node_taint_test.go
@@ -6,11 +6,14 @@ package kubernetes
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/kubernetes/pkg/util/taints"
 )
 
 const (
@@ -67,6 +70,44 @@ func TestAccKubernetesResourceNodeTaint_MultipleBasic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "taint.2.key", taintKey+"-3"),
 					resource.TestCheckResourceAttr(resourceName, "taint.2.value", taintValue),
 					resource.TestCheckResourceAttr(resourceName, "taint.2.effect", taintEffect),
+				),
+			},
+			{
+				Config: testAccKubernetesNodeTaintConfig_updateTaint(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccKubernetesNodeTaintExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "metadata.0.name"),
+					resource.TestCheckResourceAttr(resourceName, "taint.0.key", taintKey+"-1"),
+					resource.TestCheckResourceAttr(resourceName, "taint.0.value", taintValue),
+					resource.TestCheckResourceAttr(resourceName, "taint.0.effect", "NoSchedule"),
+					resource.TestCheckResourceAttr(resourceName, "taint.1.key", taintKey+"-2"),
+					resource.TestCheckResourceAttr(resourceName, "taint.1.value", taintValue),
+					resource.TestCheckResourceAttr(resourceName, "taint.1.effect", "NoSchedule"),
+					resource.TestCheckResourceAttr(resourceName, "taint.2.key", taintKey+"-3"),
+					resource.TestCheckResourceAttr(resourceName, "taint.2.value", taintValue),
+					resource.TestCheckResourceAttr(resourceName, "taint.2.effect", taintEffect),
+				),
+			},
+			{
+				Config: testAccKubernetesNodeTaintConfig_removeTaint(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccKubernetesNodeTaintExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "metadata.0.name"),
+					resource.TestCheckResourceAttr(resourceName, "taint.0.key", taintKey+"-1"),
+					resource.TestCheckResourceAttr(resourceName, "taint.0.value", taintValue),
+					resource.TestCheckResourceAttr(resourceName, "taint.0.effect", "NoSchedule"),
+					resource.TestCheckResourceAttr(resourceName, "taint.1.key", taintKey+"-2"),
+					resource.TestCheckResourceAttr(resourceName, "taint.1.value", taintValue),
+					resource.TestCheckResourceAttr(resourceName, "taint.1.effect", "NoSchedule"),
+					resource.TestCheckResourceAttr(resourceName, "taint.#", "2"),
+				),
+			},
+			{
+				Config: testAccKubernetesNodeTaintConfig_removeAllTaints(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccKubernetesNodeTaintExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "metadata.0.name"),
+					resource.TestCheckResourceAttr(resourceName, "taint.#", "0"),
 				),
 			},
 		},
@@ -163,4 +204,91 @@ resource "kubernetes_node_taint" "test" {
   field_manager = "%s"
 }
 `, taintKey, taintValue, taintEffect, taintKey, taintValue, taintEffect, taintKey, taintValue, taintEffect, fieldManager)
+}
+
+func testAccKubernetesNodeTaintConfig_updateTaint() string {
+	return fmt.Sprintf(`
+data "kubernetes_nodes" "test" {}
+
+resource "kubernetes_node_taint" "test" {
+  metadata {
+    name = data.kubernetes_nodes.test.nodes.0.metadata.0.name
+  }
+  taint {
+    key    = "%s-1"
+    value  = "%s"
+    effect = "%s"
+  }
+  taint {
+    key    = "%s-2"
+    value  = "%s"
+    effect = "%s"
+  }
+  taint {
+    key    = "%s-3"
+    value  = "%s"
+    effect = "%s"
+  }
+  field_manager = "%s"
+}
+`, taintKey, taintValue, "NoSchedule", taintKey, taintValue, "NoSchedule", taintKey, taintValue, taintEffect, fieldManager)
+}
+
+func testAccKubernetesNodeTaintConfig_removeTaint() string {
+	return fmt.Sprintf(`
+data "kubernetes_nodes" "test" {}
+
+resource "kubernetes_node_taint" "test" {
+  metadata {
+    name = data.kubernetes_nodes.test.nodes.0.metadata.0.name
+  }
+  taint {
+    key    = "%s-1"
+    value  = "%s"
+    effect = "%s"
+  }
+  taint {
+    key    = "%s-2"
+    value  = "%s"
+    effect = "%s"
+  }
+  field_manager = "%s"
+}
+`, taintKey, taintValue, "NoSchedule", taintKey, taintValue, "NoSchedule", fieldManager)
+}
+
+func testAccKubernetesNodeTaintConfig_removeAllTaints() string {
+	return fmt.Sprintf(`
+data "kubernetes_nodes" "test" {}
+
+resource "kubernetes_node_taint" "test" {
+  metadata {
+    name = data.kubernetes_nodes.test.nodes.0.metadata.0.name
+  }
+  field_manager = "%s"
+}
+`, fieldManager)
+}
+
+func hasTaint(taints []v1.Taint, taint *v1.Taint) bool {
+	for i := range taints {
+		if taint.MatchTaint(&taints[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+func idToNodeTaint(id string) (string, *v1.Taint, error) {
+	idVals := strings.Split(id, ",")
+	nodeName := idVals[0]
+	taintStr := idVals[1]
+	taints, _, err := taints.ParseTaints([]string{taintStr})
+	if err != nil {
+		return "", nil, err
+	}
+	if len(taints) == 0 {
+		return "", nil, fmt.Errorf("failed to parse taint %s", taintStr)
+	}
+	return nodeName, &taints[0], nil
 }

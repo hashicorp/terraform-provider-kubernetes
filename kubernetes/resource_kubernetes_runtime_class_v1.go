d950ec47b08af87493f0ecb0643a86543c20e2ef
sheneska.williams <sheneska.williams@hashicorp.com>
2023-04-17 16:25:59 -0400

    Add a runtime class resource (only with handler attribute)

--- kubernetes/resource_kubernetes_runtime_class_v1.go
+++ kubernetes/resource_kubernetes_runtime_class_v1.go
@@ -5,9 +5,10 @@ package kubernetes
 
 import (
 	"context"
-	// "fmt"
+	//"fmt"
 	"log"
 	"regexp"
+
 	// "time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -16,10 +17,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	nodev1 "k8s.io/api/node/v1"
+
 	// api "k8s.io/api/core/v1"
 	// "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	// pkgApi "k8s.io/apimachinery/pkg/types"
+	pkgApi "k8s.io/apimachinery/pkg/types"
 )
 
 func resourceKubernetesRuntimeClassV1() *schema.Resource {
@@ -48,9 +51,11 @@ func resourceKubernetesRuntimeClassV1() *schema.Resource {
 				Description:  "Specifies the underlying runtime and configuration that the CRI implementation will use to handle pods of this class",
 				Required:     true,
 				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^[a-z0-9]([-a-z0-9]*[a-z0-9])?`), ""),
+				ForceNew:     true,
 			},
 		},
 	}
+
 }
 
 func resourceKubernetesRuntimeClassV1Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -73,38 +78,115 @@ func resourceKubernetesRuntimeClassV1Create(ctx context.Context, d *schema.Resou
 	}
 
 	log.Printf("[INFO] New runtime class created: %#v", out)
-	//d.SetID(out.Name)
+	d.SetId(out.Name) //id of resource used in the state file, not a namespace refers to its name of the resource
 
-	return nil
+	return resourceKubernetesRuntimeClassV1Read(ctx, d, meta) //create & read goes hand in hand basically
 }
 
 func resourceKubernetesRuntimeClassV1Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// if err != nil {
-	// 	return diag.FromErr(err)
-	// }
-	// if !exists {
-	// 	d.SetId("")
-	// 	return diags
-	// }
-	// conn, err := meta.(KubeClientsets).MainClientset()
-	// if err != nil {
-	// 	return diag.FromErr(err)
-	// }
+	exists, err := resourceKubernetesRuntimeClassV1Exists(ctx, d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if !exists {
+		d.SetId("")
+		return diag.Diagnostics{}
+	}
+
+	conn, err := meta.(KubeClientsets).MainClientset()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	name := d.Id()
+
+	log.Printf("[INFO] Reading Run Time Class %s", name)
+	rc, err := conn.NodeV1().RuntimeClasses().Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		log.Printf("[DEBUG] Received error: %#v", err)
+		return diag.FromErr(err)
+	}
+
+	log.Printf("[INFO] Received Run Time Class: %#v", rc)
+	err = d.Set("metadata", flattenMetadata(rc.ObjectMeta, d, meta))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = d.Set("handler", rc.Handler)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
 	return nil
 }
 
 func resourceKubernetesRuntimeClassV1Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// conn, err := meta.(KubeClientsets).MainClientset()
-	// if err != nil {
-	// 	return diag.FromErr(err)
-	// }
-	// diags := diag.Diagnostics{}
+	conn, err := meta.(KubeClientsets).MainClientset()
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
-	// exists, err := resourceKubernetesStorageClassExists(ctx, d, meta)
+	name := d.Id()
+
+	patch := patchMetadata("metadata.0.", "/metadata/", d)
+
+	data, err := patch.MarshalJSON()
+	if err != nil {
+		return diag.Errorf("Failed to marshal update operations: %s", err)
+	}
+
+	log.Printf("[INFO] Updating run time class %s: %#v", d.Id(), patch)
+
+	out, err := conn.NodeV1().RuntimeClasses().Patch(ctx, name, pkgApi.JSONPatchType, data, metav1.PatchOptions{})
+	if err != nil {
+		return diag.Errorf("Failed to update run time class! API error: %s", err)
+	}
+
+	log.Printf("[INFO] Submitted updated run time class: %#v", out)
+	d.SetId(out.Name)
+
+	return resourceKubernetesRuntimeClassV1Read(ctx, d, meta)
+}
+
+func resourceKubernetesRuntimeClassV1Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn, err := meta.(KubeClientsets).MainClientset()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	name := d.Id()
+
+	log.Printf("[INFO] RunTimeClass: %#v", name)
+	err = conn.NodeV1().RuntimeClasses().Delete(ctx, name, metav1.DeleteOptions{})
+	if err != nil {
+		if statusErr, ok := err.(*errors.StatusError); ok && errors.IsNotFound(statusErr) {
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+	log.Printf("[INFO] RunTimeClass %s deleted", name)
 
 	return nil
 }
 
-func resourceKubernetesRuntimeClassV1Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	return nil
+// trying to get resource, if we get an error then we know it doesnt exists
+func resourceKubernetesRuntimeClassV1Exists(ctx context.Context, d *schema.ResourceData, meta interface{}) (bool, error) {
+	conn, err := meta.(KubeClientsets).MainClientset()
+	if err != nil {
+		return false, err
+	}
+
+	name := d.Id()
+
+	log.Printf("[INFO] Checking Run Time Class %s", name)
+	_, err = conn.NodeV1().RuntimeClasses().Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		if statusErr, ok := err.(*errors.StatusError); ok && errors.IsNotFound(statusErr) {
+			return false, nil
+		}
+		log.Printf("[DEBUG] Received error: %#v", err)
+	}
+
+	return true, err
 }
---
+++ kubernetes/resource_kubernetes_runtime_class_v1_test.go
@@ -0,0 +1,64 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package kubernetes
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	nodev1 "k8s.io/api/node/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestAccKubernetesruntime_class_basic(t *testing.T) {
+	var conf nodev1.RuntimeClass
+	rcName := fmt.Sprintf("tf-acc-test-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+	resourceName := "kubernetes_runtime_class.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		IDRefreshName:     resourceName,
+		IDRefreshIgnore:   []string{"metadata.0.resource_version"},
+		ProviderFactories: testAccProviderFactories,
+		//CheckDestroy:      testAccCheckKubernetesRuntimeClassDestroy,
+		Steps: []resource.TestStep{
+			//creating a run time class
+			{
+				Config: testAccKubernetesSecretConfig_immutable(rcName, true, "password"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckKubernetesruntime_classExists(resourceName, &conf),
+					resource.TestCheckResourceAttr(resourceName, "metadata.0.name", rcName),
+					resource.TestCheckResourceAttr(resourceName, "immutable", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckKubernetesruntime_classExists(n string, obj *nodev1.RuntimeClass) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		conn, err := testAccProvider.Meta().(KubeClientsets).MainClientset()
+		if err != nil {
+			return err
+		}
+		ctx := context.TODO()
+
+		out, err := conn.NodeV1().RuntimeClasses().Get(ctx, rs.Primary.ID, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+
+		*obj = *out
+		return nil
+	}
+}

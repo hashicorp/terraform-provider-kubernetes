fef8dfc2bb8973edb065fc7020f63abaf6bbd28a
BBBmau <alvarez.mauriciotm@gmail.com>
2023-07-26 13:29:49 -0700

    add resourceCheck after waitCondition

--- manifest/test/helper/state/state_helper.go
+++ manifest/test/helper/state/state_helper.go
@@ -26,6 +26,14 @@ type Helper struct {
 func NewHelper(tfstate *tfjson.State) *Helper {
 	return &Helper{tfstate}
 }
+func (s *Helper) ResourceExists(t *testing.T, resourceAddress string) bool {
+	t.Helper()
+	_, err := getAttributesValuesFromResource(s, resourceAddress)
+	if err != nil {
+		return false
+	}
+	return true
+}
 
 // getAttributesValuesFromResource pulls out the AttributeValues field from the resource at the given address
 func getAttributesValuesFromResource(state *Helper, address string) (interface{}, error) {

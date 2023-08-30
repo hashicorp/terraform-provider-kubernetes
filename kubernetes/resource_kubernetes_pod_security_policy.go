203e515a73e1b63bea0c68057ba427e3a6c31b87
BBBmau <alvarez.mauriciotm@gmail.com>
2023-08-30 10:50:51 -0700

    remove error messages in create and read functions

--- kubernetes/resource_kubernetes_pod_security_policy.go
+++ kubernetes/resource_kubernetes_pod_security_policy.go
@@ -374,20 +374,6 @@ func resourceKubernetesPodSecurityPolicyCreate(ctx context.Context, d *schema.Re
 		return diag.FromErr(err)
 	}
 
-	sv, err := serverVersionGreaterThanOrEqual(conn, "1.25.0")
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	if sv {
-		return diag.Diagnostics{
-			diag.Diagnostic{
-				Severity: diag.Error,
-				Summary:  `"PodSecurityPolicy" was removed for Kubernetes v1.25.0 and above`,
-				Detail:   `Starting from version 1.25.0 Kubernetes has removed PodSecurityPolicy entirely, leading to this resource being left empty.`,
-			},
-		}
-	}
-
 	metadata := expandMetadata(d.Get("metadata").([]interface{}))
 	spec, err := expandPodSecurityPolicySpec(d.Get("spec").([]interface{}))
 
@@ -446,20 +432,6 @@ func resourceKubernetesPodSecurityPolicyRead(ctx context.Context, d *schema.Reso
 	err = d.Set("spec", flattenedSpec)
 	if err != nil {
 		return diag.FromErr(err)
-	}
-
-	sv, err := serverVersionGreaterThanOrEqual(conn, "1.21.0")
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	if sv {
-		return diag.Diagnostics{
-			diag.Diagnostic{
-				Severity: diag.Warning,
-				Summary:  `"PodSecurityPolicy" was deprecated in Kubernetes v1.21.0`,
-				Detail:   `Starting from version 1.21.0 Kubernetes has deprecated PodSecurityPolicy and has removed it entirely in v1.25.0`,
-			},
-		}
 	}
 
 	return nil

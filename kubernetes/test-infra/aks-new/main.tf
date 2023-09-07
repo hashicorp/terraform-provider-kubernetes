6c38e7a2260988d103a4f4cc86b6eabaa052438b
Sheneska Williams <74882676+sheneska@users.noreply.github.com>
2023-09-07 12:08:43 -0400

    Enable nightly execution of AKS test (#2278)

--- kubernetes/test-infra/aks-new/main.tf
+++ kubernetes/test-infra/aks-new/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "test" {
   location            = azurerm_resource_group.test_group.location
   resource_group_name = azurerm_resource_group.test_group.name
   dns_prefix          = "test"
-  kubernetes_version  = var.kubernetes_version
+  kubernetes_version  = var.cluster_version
 
   default_node_pool {
     name       = "default"

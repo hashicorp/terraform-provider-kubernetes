232afd8d4cbb5081662819f1b20148518862f52a
BBBmau <alvarez.mauriciotm@gmail.com>
2023-09-20 10:44:30 -0700

    add match_fields

--- kubernetes/schema_affinity_spec.go
+++ kubernetes/schema_affinity_spec.go
@@ -123,6 +123,36 @@ func nodeSelectorRequirementsFields() map[string]*schema.Schema {
 				},
 			},
 		},
+		"match_fields": {
+			Type:        schema.TypeList,
+			Description: "A list of node selector requirements by node's fields. The requirements are ANDed.",
+			Optional:    true,
+			ForceNew:    true,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"key": {
+						Type:        schema.TypeString,
+						Description: "The label key that the selector applies to.",
+						Required:    true,
+						ForceNew:    true,
+					},
+					"operator": {
+						Type:        schema.TypeString,
+						Description: "A key's relationship to a set of values. Valid operators ard `In`, `NotIn`, `Exists`, `DoesNotExist`, `Gt`, and `Lt`.",
+						Required:    true,
+						ForceNew:    true,
+					},
+					"values": {
+						Type:        schema.TypeSet,
+						Description: "An array of string values. If the operator is `In` or `NotIn`, the values array must be non-empty. If the operator is `Exists` or `DoesNotExist`, the values array must be empty. This array is replaced during a strategic merge patch.",
+						Optional:    true,
+						ForceNew:    true,
+						Elem:        &schema.Schema{Type: schema.TypeString},
+						Set:         schema.HashString,
+					},
+				},
+			},
+		},
 	}
 }
 

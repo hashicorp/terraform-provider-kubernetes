c68215c95836a8837b74daceb4ccbc4d58559f2a
BBBmau <alvarez.mauriciotm@gmail.com>
2023-09-27 08:15:32 -0700

    replace schema in match_fields with function

--- kubernetes/schema_affinity_spec.go
+++ kubernetes/schema_affinity_spec.go
@@ -129,28 +129,7 @@ func nodeSelectorRequirementsFields() map[string]*schema.Schema {
 			Optional:    true,
 			ForceNew:    true,
 			Elem: &schema.Resource{
-				Schema: map[string]*schema.Schema{
-					"key": {
-						Type:        schema.TypeString,
-						Description: "The label key that the selector applies to.",
-						Required:    true,
-						ForceNew:    true,
-					},
-					"operator": {
-						Type:        schema.TypeString,
-						Description: "A key's relationship to a set of values. Valid operators ard `In`, `NotIn`, `Exists`, `DoesNotExist`, `Gt`, and `Lt`.",
-						Required:    true,
-						ForceNew:    true,
-					},
-					"values": {
-						Type:        schema.TypeSet,
-						Description: "An array of string values. If the operator is `In` or `NotIn`, the values array must be non-empty. If the operator is `Exists` or `DoesNotExist`, the values array must be empty. This array is replaced during a strategic merge patch.",
-						Optional:    true,
-						ForceNew:    true,
-						Elem:        &schema.Schema{Type: schema.TypeString},
-						Set:         schema.HashString,
-					},
-				},
+				Schema: nodeSelectorRequirementFields(),
 			},
 		},
 	}

1d59e3e01eb11e6574ce8ac08a5203baec4249bd
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2023-10-10 18:00:00 +0200

    Pin Google provider and Terraform version, and add deletion_protection

--- kubernetes/test-infra/gke/versions.tf
+++ kubernetes/test-infra/gke/versions.tf
@@ -3,5 +3,12 @@
 
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0.0, < 6.0.0"
+    }
+  }
 }

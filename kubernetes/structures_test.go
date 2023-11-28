caae0735fde3ab3bb8de2f6efc10d4af7609b869
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2023-11-28 13:35:41 +0100

    Add flattenMetadataFields unit test

--- kubernetes/structures_test.go
+++ kubernetes/structures_test.go
@@ -4,11 +4,15 @@
 package kubernetes
 
 import (
+	"reflect"
 	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestIsInternalKey(t *testing.T) {
-	testCases := []struct {
+	cases := []struct {
 		Key      string
 		Expected bool
 	}{
@@ -22,16 +26,127 @@ func TestIsInternalKey(t *testing.T) {
 		{"kubectl.kubernetes.io", true},
 		{"pv.kubernetes.io/any/path", true},
 	}
-	for _, tc := range testCases {
-		t.Run(tc.Key, func(t *testing.T) {
-			isInternal := isInternalKey(tc.Key)
-			if tc.Expected && isInternal != tc.Expected {
-				t.Fatalf("Expected %q to be internal", tc.Key)
+	for _, c := range cases {
+		t.Run(c.Key, func(t *testing.T) {
+			isInternal := isInternalKey(c.Key)
+			if c.Expected && isInternal != c.Expected {
+				t.Fatalf("Expected %q to be internal", c.Key)
 			}
-			if !tc.Expected && isInternal != tc.Expected {
-				t.Fatalf("Expected %q not to be internal", tc.Key)
+			if !c.Expected && isInternal != c.Expected {
+				t.Fatalf("Expected %q not to be internal", c.Key)
 			}
 		})
+	}
+}
+
+func TestFlattenMetadataFields(t *testing.T) {
+	t.Parallel()
+
+	annotations := map[string]string{
+		"fake.kubernetes.io": "fake",
+	}
+	labels := map[string]string{
+		"foo": "bar",
+	}
+	uid := types.UID("7e9439cb-2584-4b50-81bc-441127e11b26")
+	cases := []struct {
+		meta     metav1.ObjectMeta
+		expected []interface{}
+	}{
+		{
+			// Default namespace and static name
+			metav1.ObjectMeta{
+				Annotations:     annotations,
+				GenerateName:    "",
+				Generation:      1,
+				Labels:          labels,
+				Name:            "foo",
+				Namespace:       "",
+				ResourceVersion: "1",
+				UID:             uid,
+			},
+			[]interface{}{map[string]interface{}{
+				"annotations":      annotations,
+				"generation":       int64(1),
+				"labels":           labels,
+				"name":             "foo",
+				"resource_version": "1",
+				"uid":              uid,
+			}},
+		},
+		{
+			// Non default namespace and static name,
+			metav1.ObjectMeta{
+				Annotations:     annotations,
+				GenerateName:    "",
+				Generation:      1,
+				Labels:          labels,
+				Name:            "foo",
+				Namespace:       "Test",
+				ResourceVersion: "1",
+				UID:             uid,
+			},
+			[]interface{}{map[string]interface{}{
+				"annotations":      annotations,
+				"generation":       int64(1),
+				"labels":           labels,
+				"name":             "foo",
+				"namespace":        "Test",
+				"resource_version": "1",
+				"uid":              uid,
+			}},
+		},
+		{
+			// Default namespace and generated name
+			metav1.ObjectMeta{
+				Annotations:     annotations,
+				GenerateName:    "gen-foo",
+				Generation:      1,
+				Labels:          labels,
+				Name:            "",
+				Namespace:       "",
+				ResourceVersion: "1",
+				UID:             uid,
+			},
+			[]interface{}{map[string]interface{}{
+				"annotations":      annotations,
+				"generate_name":    "gen-foo",
+				"generation":       int64(1),
+				"labels":           labels,
+				"name":             "",
+				"resource_version": "1",
+				"uid":              uid,
+			}},
+		},
+		{
+			// Non default namespace and generated name
+			metav1.ObjectMeta{
+				Annotations:     annotations,
+				GenerateName:    "gen-foo",
+				Generation:      1,
+				Labels:          labels,
+				Name:            "",
+				Namespace:       "Test",
+				ResourceVersion: "1",
+				UID:             uid,
+			},
+			[]interface{}{map[string]interface{}{
+				"annotations":      annotations,
+				"generate_name":    "gen-foo",
+				"generation":       int64(1),
+				"labels":           labels,
+				"name":             "",
+				"namespace":        "Test",
+				"resource_version": "1",
+				"uid":              uid,
+			}},
+		},
+	}
+	for _, c := range cases {
+		out := flattenMetadataFields(c.meta)
+		if !reflect.DeepEqual(out, c.expected) {
+			t.Fatalf("Error matching output and expected: %#v vs %#v", out, c.expected)
+		}
 	}
 }
 

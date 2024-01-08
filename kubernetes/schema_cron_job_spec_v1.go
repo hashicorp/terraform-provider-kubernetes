06d0a07c533a3feddc23ec05427803f1eeaa116c
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2024-01-08 09:11:37 +0100

    Fix namespace requirement in template metadata (#2387)

--- kubernetes/schema_cron_job_spec_v1.go
+++ kubernetes/schema_cron_job_spec_v1.go
@@ -30,7 +30,7 @@ func cronJobSpecFieldsV1() map[string]*schema.Schema {
 			MaxItems:    1,
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
-					"metadata": namespacedMetadataSchema("jobTemplateSpec", true),
+					"metadata": namespacedMetadataSchemaIsTemplate("jobTemplateSpec", true, true),
 					"spec": {
 						Type:        schema.TypeList,
 						Description: "Specification of the desired behavior of the job",

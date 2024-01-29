fcfa46eab0983f5d055a6efc597c340a50145b06
BBBmau <alvarez.mauriciotm@gmail.com>
2024-01-29 15:40:05 -0800

    path typo

--- kubernetes/structure_job.go
+++ kubernetes/structure_job.go
@@ -309,7 +309,7 @@ func patchJobV1Spec(pathPrefix, prefix string, d *schema.ResourceData) PatchOper
 
 	if d.HasChange(prefix + "pod_failure_policy") {
 		ops = append(ops, &ReplaceOperation{
-			Path:  pathPrefix + "/pod_failure_policy",
+			Path:  pathPrefix + "/podFailurePolicy",
 			Value: expandPodFailurePolicy(d.Get(prefix + "pod_failure_policy").([]interface{})),
 		})
 	}

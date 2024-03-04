56c34b3725b7d8ff35dd4b9466f92dbe0346cbb5
BBBmau <alvarez.mauriciotm@gmail.com>
2024-03-04 13:13:13 -0800

    add type change check for preservedUnknownFields

--- manifest/provider/plan.go
+++ manifest/provider/plan.go
@@ -458,7 +458,8 @@ func (s *RawProviderServer) PlanResourceChange(ctx context.Context, req *tfproto
 				hasChanged = err == nil && len(restPath.Steps()) == 0 && wasCfg.(tftypes.Value).IsKnown() && !wasCfg.(tftypes.Value).Equal(nowCfg.(tftypes.Value))
 				if hasChanged {
 					h, ok := hints[morph.ValueToTypePath(ap).String()]
-					if ok && h == manifest.PreserveUnknownFieldsLabel {
+					typeChanged := !(wasCfg.(tftypes.Value).Type().Equal(nowCfg.(tftypes.Value).Type()))
+					if ok && h == manifest.PreserveUnknownFieldsLabel && typeChanged {
 						apm := append(tftypes.NewAttributePath().WithAttributeName("manifest").Steps(), ap.Steps()...)
 						resp.RequiresReplace = append(resp.RequiresReplace, tftypes.NewAttributePathWithSteps(apm))
 					}

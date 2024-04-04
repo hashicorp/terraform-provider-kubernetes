fcc2c94e84854766afba07f44e84105de83f7d68
BBBmau <alvarez.mauriciotm@gmail.com>
2024-04-04 11:00:52 -0700

    remove type check and mandatory replacement

--- manifest/provider/plan.go
+++ manifest/provider/plan.go
@@ -458,14 +458,11 @@ func (s *RawProviderServer) PlanResourceChange(ctx context.Context, req *tfproto
 				hasChanged = err == nil && len(restPath.Steps()) == 0 && wasCfg.(tftypes.Value).IsKnown() && !wasCfg.(tftypes.Value).Equal(nowCfg.(tftypes.Value))
 				if hasChanged {
 					h, ok := hints[morph.ValueToTypePath(ap).String()]
-					typeChanged := !(wasCfg.(tftypes.Value).Type().Equal(nowCfg.(tftypes.Value).Type()))
-					if ok && h == manifest.PreserveUnknownFieldsLabel && typeChanged {
-						apm := append(tftypes.NewAttributePath().WithAttributeName("manifest").Steps(), ap.Steps()...)
-						resp.RequiresReplace = append(resp.RequiresReplace, tftypes.NewAttributePathWithSteps(apm))
+					if ok && h == manifest.PreserveUnknownFieldsLabel {
 						resp.Diagnostics = append(resp.Diagnostics, &tfprotov5.Diagnostic{
 							Severity: tfprotov5.DiagnosticSeverityWarning,
-							Summary:  fmt.Sprintf("The attribute path %v value's type has changed", morph.ValueToTypePath(ap).String()),
-							Detail:   "Changes to the type will cause a forced replacement.",
+							Summary:  fmt.Sprintf("The attribute path %v value's type is an x-kubernetes-preserve-unknown-field", morph.ValueToTypePath(ap).String()),
+							Detail:   "Changes to the type may cause some unexpected behavior.",
 						})
 					}
 				}

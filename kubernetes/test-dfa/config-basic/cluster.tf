cb6fd794b334e11f8f3b1f10605032ce86c4ca72
Alex Somesan <alex.somesan@gmail.com>
2024-05-29 19:54:02 +0200

    Deferred actions acc test

---
+++ kubernetes/test-dfa/config-basic/cluster.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "tehcyx/kind"
+    }
+  }
+}
+
+resource "kind_cluster" "demo" {
+  name = "tfacc"
+}
+
+provider "kubernetes" {
+  host                   = kind_cluster.demo.endpoint
+  cluster_ca_certificate = kind_cluster.demo.cluster_ca_certificate
+  client_certificate     = kind_cluster.demo.client_certificate
+  client_key             = kind_cluster.demo.client_key
+}
---
+++ kubernetes/test-dfa/deferred_actions_test.go
@@ -0,0 +1,112 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package testdfa
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-provider-kubernetes/internal/mux"
+)
+
+var muxFactory = map[string]func() (tfprotov5.ProviderServer, error){
+	"kubernetes": func() (tfprotov5.ProviderServer, error){
+		return mux.MuxServer(context.Background(), "Test")
+	},
+}
+
+func TestAccKubernetesDeferredActions_2_step(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		AdditionalCLIOptions: &resource.AdditionalCLIOptions{
+			Plan: resource.PlanOptions{AllowDeferral: true},
+			Apply: resource.ApplyOptions{AllowDeferral: true},
+		},
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: muxFactory,
+				ConfigDirectory: func(tscr config.TestStepConfigRequest) string {
+					return "./config-basic"
+				},
+				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("kind_cluster.demo", plancheck.ResourceActionCreate),
+						plancheck.ExpectDeferredChange("kubernetes_namespace_v1.demo_ns", plancheck.DeferredReasonProviderConfigUnknown),
+						plancheck.ExpectDeferredChange("kubernetes_manifest.crd_workspaces", plancheck.DeferredReasonProviderConfigUnknown),
+						plancheck.ExpectDeferredChange("kubernetes_manifest.demo_workspace", plancheck.DeferredReasonProviderConfigUnknown),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("kubernetes_namespace_v1.demo_ns", plancheck.ResourceActionCreate),
+						plancheck.ExpectResourceAction("kubernetes_manifest.crd_workspaces", plancheck.ResourceActionCreate),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("endpoint"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("cluster_ca_certificate"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("client_certificate"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("client_key"), knownvalue.NotNull()),
+				},
+			},
+			{
+				ProtoV5ProviderFactories: muxFactory,
+				ConfigDirectory: func(tscr config.TestStepConfigRequest) string {
+					return "./config-basic"
+				},
+				ExpectNonEmptyPlan: true,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("kubernetes_namespace_v1.demo_ns", plancheck.ResourceActionCreate),
+						plancheck.ExpectDeferredChange("kubernetes_manifest.demo_workspace", plancheck.DeferredReasonResourceConfigUnknown),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("kubernetes_manifest.demo_workspace", plancheck.ResourceActionCreate),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("endpoint"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("cluster_ca_certificate"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("client_certificate"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("client_key"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kubernetes_namespace_v1.demo_ns", tfjsonpath.New("metadata").AtSliceIndex(0).AtMapKey("name"), knownvalue.StringExact("demo-ns")),
+					statecheck.ExpectKnownValue("kubernetes_manifest.crd_workspaces", tfjsonpath.New("manifest"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kubernetes_manifest.crd_workspaces", tfjsonpath.New("object"), knownvalue.NotNull()),
+				},
+			},
+			{
+				ProtoV5ProviderFactories: muxFactory,
+				ConfigDirectory: func(tscr config.TestStepConfigRequest) string {
+					return "./config-basic"
+				},
+				ExpectNonEmptyPlan: false,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectNoDeferredChanges(),
+						plancheck.ExpectResourceAction("kubernetes_manifest.demo_workspace", plancheck.ResourceActionCreate),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectEmptyPlan(),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("endpoint"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("cluster_ca_certificate"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("client_certificate"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kind_cluster.demo", tfjsonpath.New("client_key"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kubernetes_namespace_v1.demo_ns", tfjsonpath.New("metadata").AtSliceIndex(0).AtMapKey("name"), knownvalue.StringExact("demo-ns")),
+					statecheck.ExpectKnownValue("kubernetes_manifest.crd_workspaces", tfjsonpath.New("manifest"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kubernetes_manifest.crd_workspaces", tfjsonpath.New("object"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kubernetes_manifest.demo_workspace", tfjsonpath.New("manifest"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue("kubernetes_manifest.demo_workspace", tfjsonpath.New("object"), knownvalue.NotNull()),
+				},
+			},
+		},
+	})
+}

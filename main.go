d567b7e7632339d3fde5bfaed9c872e8da1aacca
Alex Pilon <apilon@hashicorp.com>
2024-07-24 11:53:36 -0400

    Fix main.go

--- main.go
+++ main.go
@@ -16,15 +16,13 @@ import (
 	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
+	tf5server "github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
-
-	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
 	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
 	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
-
 	framework "github.com/hashicorp/terraform-provider-kubernetes/internal/framework/provider"
+	"github.com/hashicorp/terraform-provider-kubernetes/internal/mux"
 	"github.com/hashicorp/terraform-provider-kubernetes/kubernetes"
 	manifest "github.com/hashicorp/terraform-provider-kubernetes/manifest/provider"
 )
@@ -47,14 +45,9 @@ func main() {
 	debugFlag := flag.Bool("debug", false, "Start provider in stand-alone debug mode.")
 	flag.Parse()
 
-	providers := []func() tfprotov5.ProviderServer{
-		kubernetes.Provider().GRPCProvider,
-		manifest.Provider(),
-		providerserver.NewProtocol5(framework.New(Version)),
-	}
-
 	ctx := context.Background()
-	muxer, err := tf5muxserver.NewMuxServer(ctx, providers...)
+
+	muxer, err := mux.MuxServer(ctx, Version)
 	if err != nil {
 		log.Println(err.Error())
 		os.Exit(1)
@@ -74,7 +67,7 @@ func main() {
 		opts = append(opts, tf5server.WithDebug(ctx, reattachConfigCh, nil))
 	}
 
-	tf5server.Serve(providerName, muxer.ProviderServer, opts...)
+	tf5server.Serve(providerName, func() tfprotov5.ProviderServer { return muxer }, opts...)
 }
 
 func plugin6main() {

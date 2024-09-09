80e9df7ea4dd69db0f7eaee835e9926e4589a4c4
Alex Pilon <apilon@hashicorp.com>
2024-09-09 17:25:46 -0400

    Fixes
    - Add KUBE_CONFIG_PATHS (plural) to testAccPreCheck
    - Make singleNestedPath heuristic more robust
    - Enable more test cases

--- kubernetes/provider_test.go
+++ kubernetes/provider_test.go
@@ -161,7 +161,8 @@ func testAccPreCheck(t *testing.T) {
 	ctx := context.TODO()
 	hasFileCfg := (os.Getenv("KUBE_CTX_AUTH_INFO") != "" && os.Getenv("KUBE_CTX_CLUSTER") != "") ||
 		os.Getenv("KUBE_CTX") != "" ||
-		os.Getenv("KUBE_CONFIG_PATH") != ""
+		os.Getenv("KUBE_CONFIG_PATH") != "" ||
+		os.Getenv("KUBE_CONFIG_PATHS") != ""
 	hasUserCredentials := os.Getenv("KUBE_USER") != "" && os.Getenv("KUBE_PASSWORD") != ""
 	hasClientCert := os.Getenv("KUBE_CLIENT_CERT_DATA") != "" && os.Getenv("KUBE_CLIENT_KEY_DATA") != ""
 	hasStaticCfg := (os.Getenv("KUBE_HOST") != "" &&

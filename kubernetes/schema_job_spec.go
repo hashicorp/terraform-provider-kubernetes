9d6074c763ed9d112c3f6145e30235a9b0758c4c
theloneexplorerquest <150489859+theloneexplorerquest@users.noreply.github.com>
2024-09-21 11:27:07 +1000

    Support JobBackoffLimitPerIndex feature gate fields (#2421)
    * update job spec
    
    * fix tests by adding in extra checks and DiffSuppressFuncs for both new attributes
    
    * add changelog
    
    * add docs
    
    * add new fields into TestAccKubernetesCronJobV1_minimalWithPodFailurePolicy test
    
    * insert field checks for new fields
    
    * make tests
    
    * add flatteners
    
    * lint fix
    
    * add new test - TestAccKubernetesCronJobV1_minimalWithBackoffLimitPerIndex
    
    * remove fields from other test
    
    ---------
    
    Co-authored-by: Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
    Co-authored-by: BBBmau <alvarez.mauriciotm@gmail.com>

--- kubernetes/schema_job_spec.go
+++ kubernetes/schema_job_spec.go
@@ -57,6 +57,13 @@ func jobSpecFields(specUpdatable bool) map[string]*schema.Schema {
 			ValidateFunc: validateNonNegativeInteger,
 			Description:  "Specifies the number of retries before marking this job failed. Defaults to 6",
 		},
+		"backoff_limit_per_index": {
+			Type:         schema.TypeInt,
+			Optional:     true,
+			ForceNew:     true,
+			ValidateFunc: validateNonNegativeInteger,
+			Description:  "Specifies the limit for the number of retries within an index before marking this index as failed. When enabled the number of failures per index is kept in the pod's batch.kubernetes.io/job-index-failure-count annotation. It can only be set when Job's completionMode=Indexed, and the Pod's restart policy is Never. The field is immutable.",
+		},
 		// This field is immutable in Jobs.
 		"completions": {
 			Type:         schema.TypeInt,
@@ -82,6 +89,13 @@ func jobSpecFields(specUpdatable bool) map[string]*schema.Schema {
 			Optional:    true,
 			ForceNew:    false,
 			Description: "Controls generation of pod labels and pod selectors. Leave unset unless you are certain what you are doing. When false or unset, the system pick labels unique to this job and appends those labels to the pod template. When true, the user is responsible for picking unique labels and specifying the selector. Failure to pick a unique label may cause this and other jobs to not function correctly. More info: https://git.k8s.io/community/contributors/design-proposals/selector-generation.md",
+		},
+		"max_failed_indexes": {
+			Type:         schema.TypeInt,
+			Optional:     true,
+			ForceNew:     false,
+			ValidateFunc: validateNonNegativeInteger,
+			Description:  "Controls generation of pod labels and pod selectors. Leave unset unless you are certain what you are doing. When false or unset, the system pick labels unique to this job and appends those labels to the pod template. When true, the user is responsible for picking unique labels and specifying the selector. Failure to pick a unique label may cause this and other jobs to not function correctly. More info: https://git.k8s.io/community/contributors/design-proposals/selector-generation.md",
 		},
 		"parallelism": {
 			Type:         schema.TypeInt,
